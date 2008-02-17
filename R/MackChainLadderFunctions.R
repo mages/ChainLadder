46b9f402049935f6cb730a5d0c04108eec6da3d6
markus.gesmann <markus.gesmann@e5f87290-e53f-0410-917e-5545ed4be688>
2008-02-17 22:11:14 +0000

    Initial import
    git-svn-id: https://chainladder.googlecode.com/svn/trunk@6 e5f87290-e53f-0410-917e-5545ed4be688

---
+++ R/MackChainLadderFunctions.R
@@ -0,0 +1,267 @@
+
+# Author: Markus Gesmann
+# Copyright: Markus Gesmann, markus.gesmann@web.de
+# Date:10/11/2007
+
+MackChainLadder <- function(Triangle, weights=1/Triangle){
+  	
+  n <- ncol(Triangle)
+  m <- nrow(Triangle)
+
+if(n!=m){
+  print(dim(Triangle))	
+  stop("Number of origin years has to be equal to number of development years.\n")	
+}
+  myModel <- vector("list", (n-1))
+  for(i in c(1:(n-1))){
+  	# weighted linear regression through origin
+    x <- Triangle[1:(m-i),i]
+   	y <- Triangle[1:(m-i),i+1]
+      	
+  	myModel[[i]] <- lm(y~x+0, weights=weights[1:(m-i),i], data=data.frame(x,y))   	
+ 	 }
+ 	 
+ 	 # Predict the chain ladder model
+   FullTriangle <- predict.TriangleModel(list(Models=myModel, Triangle=Triangle))
+   
+   # Estimate the standard error
+   StdErr <- Mack.S.E(myModel, FullTriangle)
+  
+  #Collect the output                  
+  output <- list()
+  
+  output[["Triangle"]] <- Triangle
+  output[["FullTriangle"]] <- FullTriangle
+  output[["Models"]] <- myModel
+  output[["f"]] <- StdErr$f
+  output[["f.se"]] <- StdErr$f.se
+  output[["F.se"]] <- StdErr$F.se # needs to be put into documentation
+  output[["sigma"]] <- StdErr$sigma
+  output[["Mack.S.E"]] <- StdErr$FullTriangle.se
+  output[["Total.Mack.S.E"]] <- TotalMack.seR(FullTriangle, StdErr$f, StdErr$f.se, StdErr$F.se)
+  
+  class(output) <- c("MackChainLadder", "TriangleModel", "list")
+  return(output)
+}
+
+###############################################################################
+# predict
+#	
+predict.TriangleModel <- function(object,...){
+  n <- ncol(object[["Triangle"]])
+  m <- nrow(object[["Triangle"]])
+  FullTriangle <- object[["Triangle"]]
+  for(j in c(1:(n-1))){
+    	for(k in c((n-j+1):m)){
+      	FullTriangle[k, j+1] <- predict(object[["Models"]][[j]], 
+                                 newdata=data.frame(x=FullTriangle[k, j]),...)                              	
+       }
+  }                               
+  return(FullTriangle)
+}
+
+##############################################################################
+# Calculation of the mean squared error and standard error
+# mean squared error = stochastic error (process variance) + estimation error
+# standard error = sqrt(mean squared error)
+ 
+ Mack.S.E <- function(MackModel, FullTriangle){
+   n <- ncol(FullTriangle)
+   m <- nrow(FullTriangle)
+   f <- rep(1,n)
+   f.se <- rep(0,n)
+   sigma <- rep(0,(n-1))
+
+   # Extract estimated slopes, std. error and sigmas
+   f[1:(n-1)] <- sapply(MackModel, function(x) summary(x)$coef["x","Estimate"])
+   f.se[1:(n-1)] <- sapply(MackModel, function(x) summary(x)$coef["x","Std. Error"])
+   sigma[1:(n-1)] <- sapply(MackModel, function(x) summary(x)$sigma)
+   
+  
+  # estimate sigma[n-1] via log-linear regression
+  dev=c(1:(n-2))
+  gn <- which(sigma>0)
+  .sigma <- sigma[gn]
+  .dev <- dev[gn]
+  sigmaModel <- lm(log(.sigma) ~ .dev)
+  sigma[n-1] <- exp(predict(sigmaModel, newdata=data.frame(.dev=(n-1))))
+  
+  f.se[n-1] = sigma[n-1]/sqrt(FullTriangle[1,n-1]) 
+  
+  F.se <- t(t(1/sqrt(FullTriangle)[,-n])*(sigma))
+  
+  FullTriangle.se <- FullTriangle * 0
+  # Recursive Formula
+  for(i in 2:m){
+    for(k in c((n+1-i):(n-1))){
+    	 FullTriangle.se[i,k+1] = sqrt(
+    	   FullTriangle[i,k]^2*(F.se[i,k]^2+f.se[k]^2) # 
+    	   + FullTriangle.se[i,k]^2*f[k]^2
+    	 )
+    	}	
+  	}
+  return(list(sigma=sigma, f=f, f.se=f.se, FullTriangle.se=FullTriangle.se, F.se=F.se) )
+  }
+##############################################################################
+# Summary
+#
+summary.MackChainLadder <- function(object,...){
+# Summarise my results
+  .Triangle <- object[["Triangle"]]
+  n <- ncol(.Triangle)
+  m <- nrow(.Triangle)
+  
+  Latest <- rev(.Triangle[row(as.matrix(.Triangle)) == (m+1 - col(as.matrix(.Triangle)))])  
+  Ultimate <- object[["FullTriangle"]][,n]
+  Dev.To.Date <- Latest/Ultimate
+  Reserve <- Ultimate-Latest
+  Mack.S.E <- object[["Mack.S.E"]][,n]  
+  Mack.S.E.Ratio <- Mack.S.E/(Ultimate-Latest)
+
+  myResult <- data.frame(Latest, Dev.To.Date, Ultimate, Reserve, Mack.S.E, Mack.S.E.Ratio) 
+
+  return(myResult)
+}
+
+##############################################################################
+# print 
+#
+print.MackChainLadder <- function(x,...){
+   res <- summary(x)
+	print(format(res, big.mark = ",", digits = 3),...)
+
+  Totals <-  c(sum(res$Latest,na.rm=TRUE), sum(res$Ultimate,na.rm=TRUE),
+               sum(res$Reserve,na.rm=TRUE), x[["Total.Mack.S.E"]],
+               x[["Total.Mack.S.E"]]/sum(res$Reserve,na.rm=TRUE)*100)
+  Totals <- formatC(Totals, big.mark=",",digit=0,format="f")
+  Totals <- as.data.frame(Totals)
+  colnames(Totals)=c("Totals:")
+  rownames(Totals) <- c("Sum of Latest:","Sum of CL-Ultimate:",
+                        "Sum of CL-Reserve:","Total Mack S.E.:",
+                        "Total S.E.% of Reserve:")
+  cat("\n")
+  print(Totals, quote=FALSE)
+
+
+	}
+
+
+################################################################################
+# plot
+#
+plot.MackChainLadder <- function(x, mfrow=c(3,2), title=NULL,...){
+
+ if(is.null(title)) myoma <- c(0,0,0,0) else myoma <- c(0,0,2,0)
+
+ op=par(mfrow=mfrow, oma=myoma)
+
+  .myResult <- summary(x)
+  .FullTriangle <- x[["FullTriangle"]]
+  .Triangle <- x[["Triangle"]]
+  n <- nrow(.Triangle)
+  bp <- barplot(t(as.matrix(.myResult[,c("Latest","Reserve")])), 
+  		legend.text=c("Latest","Reserve"),
+  		names.arg=c(1:n),
+  		main="Mack Chain Ladder Results",
+  		xlab="Origin year",
+  		ylab="Amounts",#paste(Currency,myUnit), 
+  		ylim=c(0, max(.myResult$Ultimate+.myResult$Mack.S.E)))
+  		
+  # add error ticks
+  require("Hmisc")		
+  errbar(x=bp, y=.myResult$Ultimate, 
+  	yplus=(.myResult$Ultimate + .myResult$Mack.S.E), 
+  	yminus=(.myResult$Ultimate - .myResult$Mack.S.E),
+  	cap=0.05,
+  	add=TRUE)
+  
+  matplot(t(.FullTriangle), t="l", 
+    main="Chain ladder developments by origin year",
+    xlab="Development year", 
+    ylab="Amounts", #paste(Currency, myUnit)
+    )
+  matplot(t(.Triangle), add=TRUE)
+  
+  Residuals=residuals(x)
+    plot(standard.residuals ~ fitted.value, data=Residuals,
+    	ylab="Standardised residuals", xlab="Fitted")
+    	lines(lowess(Residuals$fitted.value, Residuals$standard.residuals), col="red")
+    	abline(h=0, col="grey") 
+    plot(standard.residuals ~ origin.year, data=Residuals,
+    	ylab="Standardised residuals", xlab="Origin year")
+    	lines(lowess(Residuals$origin.year, Residuals$standard.residuals), col="red")
+    	abline(h=0, col="grey")
+    plot(standard.residuals ~ cal.year, data=Residuals,
+    	ylab="Standardised residuals", xlab="Calendar year")
+    	lines(lowess(Residuals$cal.year, Residuals$standard.residuals), col="red")
+      abline(h=0, col="grey")
+    plot(standard.residuals ~ dev.year, data=Residuals,
+    	ylab="Standardised residuals", xlab="Development year")
+    	lines(lowess(Residuals$dev.year, Residuals$standard.residuals), col="red")
+  	abline(h=0, col="grey")
+
+  title( title , outer=TRUE) 
+  par(op)
+}
+################################################################################
+# residuals
+#
+residuals.MackChainLadder <- function(object,...){
+   n <- nrow(object[["Triangle"]])
+   myresiduals <- unlist(lapply(object[["Models"]], resid,...))
+   standard.residuals <- unlist(lapply(object[["Models"]], rstandard,...))
+   fitted.value <- unlist(lapply(object[["Models"]], fitted)) 
+   origin.year <- unlist(lapply(1:(n-1), function(x) 1:length(resid( object[["Models"]][[x]]) )))
+   dev.year <- unlist(lapply(1:(n-1), function(x) rep(x,length(resid( object[["Models"]][[x]]) ))))
+   cal.year <- origin.year + dev.year - 1
+   
+   myResiduals=data.frame(origin.year,
+                          dev.year,
+                          cal.year,
+                          residuals=myresiduals, 
+                          standard.residuals,
+                          fitted.value)
+   return(na.omit(myResiduals))
+   }
+ 
+ 
+ ## work on update
+
+################################################################################
+## Total reserve SE
+
+TotalMack.seR <- function(FullTriangle,f, f.se, F.se){
+
+  C <- FullTriangle
+  n <- ncol(C)
+  m <- nrow(C)
+  
+  total.seR <- 0*c(1:(n))
+#  q <- 0
+#  dev <- 1
+#  i <- 1
+  # IF Quarterly
+  
+  for(k in c(1:(n-1))){
+    total.seR[k+1] <- sqrt(total.seR[k]^2 * f[k]^2 +
+                           sum(C[c((m+1-k):m),k]^2 *
+                               (F.se[c((m+1-k):m),k]^2))
+                           + sum(C[c((m+1-k):m),k])^2 * f.se[k]^2 )
+#    if(k%%dev == q){
+#      i <- i+1
+#    }
+  }
+  # if tail factor exsits, calculate the ultimate 
+ # if(f[n] > 1){
+ #   
+ #   se.f[n] <- mack.se.fult(clratios=f, se.f=se.f)
+ #   se.F[,n] <- mack.se.Fult(se.fult=se.f[n], se.F=se.F)
+ #   
+ #   total.seR[n] <- sqrt(total.seR[n]^2 * f[n]^2 +
+ #                        sum(C[c(1:m),n]^2 *
+ #                            (se.F[c(1:m),n]^2))
+ #                        + sum(C[c(1:m),n])^2 * se.f[n]^2 )
+ # }
+ #
+  return(total.seR[length(total.seR)])
+}
---
+++ R/MunichChainLadderFunctions.R
@@ -0,0 +1,193 @@
+# Author: Markus Gesmann
+# Copyright: Markus Gesmann, markus.gesmann@web.de
+# Date:10/11/2007
+
+####################################################
+# Get the top left triangle of a matrix
+left.tri <- function(x) col(as.matrix(x)) < ncol(as.matrix(x))-row(as.matrix(x)) + 2
+####################################################
+
+##############################################################################
+# Munich Chain Ladder
+#
+MunichChainLadder <- function(Paid, Incurred){
+	
+ if(!all(dim(Paid) == dim(Incurred)))
+ 	stop("Paid and Incurred triangle must have same dimension.\n")
+ if(nrow(Paid) != ncol(Paid))
+ 	stop("Number of origin years has to be equal to number of development years.\n")	
+	
+
+ MackPaid = MackChainLadder(Paid)
+ MackIncurred = MackChainLadder(Incurred)
+
+ n <- ncol(Paid)
+
+ FullPaid = MackPaid[["FullTriangle"]]
+ FullIncurred = MackIncurred[["FullTriangle"]]
+
+
+ myQModel <- vector("list", n)
+ q.f <- rep(1,n)
+ rhoI.sigma <- rep(0,n)
+
+ for(s in c(1:n)){
+ 	myQModel[[s]] <- lm(Paid[1:(n-s+1),s] ~ Incurred[1:(n-s+1),s] + 0,
+					weight=1/Incurred[1:(n-s+1),s])
+
+	q.f[s] = summary(myQModel[[s]])$coef[1]
+	rhoI.sigma[s] = summary(myQModel[[s]])$sigma
+ }
+
+
+ myQinverseModel <- vector("list", n)
+ qinverse.f <- rep(1,n)
+ rhoP.sigma <- rep(0,n)
+
+ for(s in c(1:n)){
+ 	myQinverseModel[[s]] <- lm(Incurred[1:(n-s+1),s] ~ Paid[1:(n-s+1),s] + 0,
+					weight=1/Paid[1:(n-s+1),s])
+
+	qinverse.f[s] = summary(myQinverseModel[[s]])$coef[1]
+	rhoP.sigma[s] = summary(myQinverseModel[[s]])$sigma
+ }
+
+  # Estimate the residuals
+
+  Paidf <-  t(matrix(rep(MackPaid$f[-n],(n-1)), ncol=(n-1)))
+  PaidSigma <- t(matrix(rep(MackPaid$sigma,(n-1)), ncol=(n-1)))
+  PaidRatios <- Paid[-n,-1]/Paid[-n,-n]
+  PaidResiduals <- (PaidRatios - Paidf)/PaidSigma * sqrt(Paid[-n,-n])
+
+  Incurredf <-  t(matrix(rep(MackIncurred$f[-n],(n-1)), ncol=(n-1)))
+  IncurredSigma <- t(matrix(rep(MackIncurred$sigma,(n-1)), ncol=(n-1)))
+  IncurredRatios <- Incurred[-n,-1]/Incurred[-n,-n]
+  IncurredResiduals <- (IncurredRatios - Incurredf)/IncurredSigma * sqrt(Incurred[-n,-n])
+
+
+  QRatios <- (Paid/Incurred)[,-n]
+  Qf <- t(matrix(rep(q.f[-n],n), ncol=n))
+  QSigma <- t(matrix(rep(rhoI.sigma[-n],n), ncol=n))
+  QResiduals <- (QRatios - Qf)/QSigma * sqrt(Incurred[,-n])
+
+  QinverseRatios <- 1/QRatios
+  Qinversef <- 1/Qf
+  QinverseSigma <- t(matrix(rep(rhoP.sigma[-n],n), ncol=n))
+  QinverseResiduals <- (QinverseRatios - Qinversef)/QinverseSigma * sqrt(Paid[,-n])
+
+
+ # linear regression of the residuals through the origin
+ lambdaP <- coef(lm(QinverseResiduals[left.tri(QinverseResiduals)] ~ PaidResiduals[left.tri(PaidResiduals)] +0))
+ lambdaI <- coef(lm(QResiduals[left.tri(QResiduals)] ~ IncurredResiduals[left.tri(IncurredResiduals)] +0))
+
+
+ # Recursive Munich Chain Ladder Forumla
+ FullPaid <- Paid
+ FullIncurred <- Incurred
+ for(j in c(1:(n-1))){
+  for(i in c((n-j+1):n) ){
+   # Paid
+   mclcorrection <- lambdaP*MackPaid$sigma[j]/rhoP.sigma[j]*(
+   	FullIncurred[i,j]/FullPaid[i,j]-qinverse.f[j]
+   	)
+   FullPaid[i,j+1] = FullPaid[i,j] * (MackPaid$f[j] + mclcorrection)
+   # Incurred
+   mclcorrection <- lambdaI*MackIncurred$sigma[j]/rhoI.sigma[j]*(
+   	FullPaid[i,j]/FullIncurred[i,j]-q.f[j]
+   	)
+   FullIncurred[i,j+1] = FullIncurred[i,j] * (MackIncurred$f[j] + mclcorrection)
+
+     }
+   }
+
+   output <- list()
+   output[["Paid"]] <- Paid
+   output[["Incurred"]] <- Incurred
+   output[["MCLPaid"]] <- FullPaid
+   output[["MCLIncurred"]] <- FullIncurred
+   output[["SCLPaid"]] <- MackPaid
+   output[["SCLIncurred"]] <- MackIncurred
+   output[["PaidResiduals"]] <-  PaidResiduals
+   output[["IncurredResiduals"]] <- IncurredResiduals
+   output[["QResiduals"]] <- QResiduals
+   output[["QinverseResiduals"]] <- QinverseResiduals
+   output[["lambdaP"]] <- lambdaP
+   output[["lambdaI"]] <- lambdaI
+
+
+   class(output) <- c("MunichChainLadder", "list")
+
+   return(output)
+ 
+ }
+
+##############################################################################
+# summary 
+#
+summary.MunichChainLadder <- function(object,...){
+   n <- ncol(object[["MCLPaid"]])	
+   .Paid <- as.matrix(object[["Paid"]])
+   .Incurred <- as.matrix(object[["Incurred"]])   
+
+  	LatestPaid = rev(.Paid[row(.Paid) == (n+1 - col(.Paid))])
+    LatestIncurred = rev(.Incurred[row(.Incurred) == (n+1 - col(.Incurred))])
+    UltimatePaid = object[["MCLPaid"]][,n]
+    UltimateIncurred = object[["MCLIncurred"]][,n]
+   	
+       Result <- data.frame(LatestPaid,	LatestIncurred, 
+       				Latest.P.I.Ratio=LatestPaid/LatestIncurred,
+       				UltimatePaid,
+       				UltimateIncurred,
+       				Ultimate.P.I.Ratio=UltimatePaid/UltimateIncurred)
+	return(Result)
+}
+
+##############################################################################
+# print 
+#	
+ print.MunichChainLadder <- function(x,...){
+  	print(format(summary(x), big.mark = ",", digits = 3),...)
+ }
+
+##############################################################################
+# plot 
+#
+ plot.MunichChainLadder <- function(x, mfrow=c(2,2), title=NULL, ...){
+
+ if(is.null(title)) myoma <- c(0,0,0,0) else myoma <- c(0,0,2,0)
+
+ op=par(mfrow=mfrow, oma=myoma)
+ 
+  n <- ncol(x[["MCLPaid"]])
+  barplot(t(as.matrix(data.frame(Paid=x[["MCLPaid"]][,n], Incurred=x[["MCLIncurred"]][,n]))),
+  beside=TRUE, legend.text=c("MCL Paid", "MCL Incurred"), names.arg=c(1:n),
+  xlab="origin year", ylab="Amounts", main="Munich Chain Ladder Results")
+
+  barplot(t(as.matrix(
+  data.frame(SCL.PI=100*x[["SCLPaid"]]$FullTriangle[,n]/x[["SCLIncurred"]]$FullTriangle[,n],
+  			MCL.PI=100*x[["MCLPaid"]][,n]/x[["MCLIncurred"]][,n]))),
+  beside=TRUE, legend.text=c("SCL P/I", "MCL P/I"), names.arg=c(1:n),
+  xlab="origin year", ylab="%", main="Munich Chain Ladder vs. Standard Chain Ladder")
+
+
+  plot(x[["PaidResiduals"]][left.tri(x[["PaidResiduals"]])] ~ x[["QinverseResiduals"]][left.tri(x[["QinverseResiduals"]])],
+    xlim=c(-2,2), ylim=c(-2,2),
+    xlab="Paid residuals",
+    ylab="Incurred/Paid residuals",
+    main="Paid residual plot")
+	abline(v=0)
+	abline(h=0)
+	abline(a=0,b=x[["lambdaP"]], col="red")
+
+  plot(x[["IncurredResiduals"]][left.tri(x[["IncurredResiduals"]])] ~ x[["QResiduals"]][left.tri(x[["QResiduals"]])],
+    xlim=c(-2,2), ylim=c(-2,2),
+    xlab="Incurred residuals",
+    ylab="Paid/Incurred residuals",
+    main="Incurred residual plot")
+	abline(v=0)
+	abline(h=0)
+	abline(a=0,b=x[["lambdaI"]], col="red")
+
+  title( title , outer=TRUE) 
+  par(op)
+  }
