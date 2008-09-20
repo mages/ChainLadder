51afb6f3fcaae9e7c5dff8a2c278877b70a8dd3a
markus.gesmann <markus.gesmann@e5f87290-e53f-0410-917e-5545ed4be688>
2008-09-20 23:10:52 +0000

    Add BootstrapReserve.R and ChainLadder.R
    git-svn-id: https://chainladder.googlecode.com/svn/trunk@56 e5f87290-e53f-0410-917e-5545ed4be688

---
+++ R/BootstrapReserve.R
@@ -0,0 +1,297 @@
+## Author: Markus Gesmann
+## Copyright: Markus Gesmann, markus.gesmann@gmail.com
+## Date:19/09/2008
+
+
+## source(paste(searchpaths()[grep("ChainLadder", searchpaths())],"/Experimental/BootstrapReserve.R", sep=""))
+
+BootReserve <- function(Triangle = RAA, R = 1000, process.distr="gamma"){
+
+    triangle <- Triangle
+   # if(nrow(triangle) != ncol(triangle))
+# 	stop("Number of origin years has to be equal to number of development years.\n")
+
+    ## Obtain the standard chain-ladder development factors from cumulative data.
+
+    n <- ncol(triangle)
+    triangle <- array(triangle, dim=c(n,n,1))
+    inc.triangle <- getIncremental(triangle)
+
+    lobs <- row(triangle[,,1]) == (n+1 - col(triangle[,,1]))
+    obs <- row(triangle[,,1]) <= (n+1 - col(triangle[,,1]))
+    Latest <- getLatest(inc.triangle)
+
+    ## Obtain cumulative fitted values for the past triangle by backwards
+    ## recursion, starting with the observed cumulative paid to date in the latest
+    ## diagonal
+
+    dfs <- getIndivDFs(triangle)
+    weights <- triangle
+    avDFs <- getAvDFs(dfs, weights)
+    ultDFs <- getUltDFs(avDFs)
+    ults <- getUltimates(Latest, ultDFs)
+    ## Obtain incremental fitted values, m^ ij, for the past triangle by differencing.
+    exp.inc.triangle <- getIncremental(getExpected(ults, 1/ultDFs))
+    exp.inc.triangle[is.na(inc.triangle[,,1])] <- NA
+
+    ## Calculate the unscaled Pearson residuals for the past triangle using:
+    unscaled.residuals  <- (inc.triangle - exp.inc.triangle)/sqrt(abs(exp.inc.triangle))
+
+    ## Calculate the Pearson scale parameter
+    nobs  <- sum(1:n)
+    scale.factor <- (0.5*n*(n+1)-2*n+1)
+    scale.phi <- sum(unscaled.residuals^2,na.rm=TRUE)/scale.factor
+    ## Adjust the Pearson residuals using
+    adj.resids <- unscaled.residuals * sqrt(nobs/scale.factor)
+
+
+    ## Sample incremental claims
+    ## Resample the adjusted residuals with replacement, creating a new
+    ## past triangle of residuals.
+
+    simClaims <- randomClaims(exp.inc.triangle, adj.resids, R)
+
+    ## Fit the standard chain-ladder model to the pseudo-cumulative data.
+    ## Project to form a future triangle of cumulative payments.
+
+    ## Perform chain ladder projection
+    simLatest <- getLatest(simClaims)
+    simCum <- makeCumulative(simClaims)
+    simDFs <- getIndivDFs(simCum)
+    simWghts <- simCum
+    simAvDFs <- getAvDFs(simDFs, simWghts)
+    simUltDFs <- getUltDFs(simAvDFs)
+    simUlts <- getUltimates(simLatest, simUltDFs)
+    ## Get expected future claims
+    ## Obtain the corresponding future triangle of incremental payments by
+    ## differencing, to be used as the mean when simulating from the process
+    ## distribution.
+
+    simExp <- getIncremental(getExpected(simUlts, 1/simUltDFs))
+    simExp[!is.na(simClaims)] <- NA
+
+    if(process.distr=="gamma")
+        processTriangle <-  apply(simExp,c(1,2,3), function(x)
+                                  ifelse(is.na(x), NA, sign(x)*rgamma(1, shape=abs(x/scale.phi), scale=scale.phi)))
+    if(process.distr=="od.pois")
+        processTriangle <-  apply(simExp,c(1,2,3), function(x)
+                                  ifelse(is.na(x), NA, sign(x)*rpois.od(1, abs(x), scale.phi)))
+    ##if(process.distr=="od.nbionm")
+    ##  processTriangle <-  apply(simExp,c(1,2,3), function(x)
+    ##          ifelse(is.na(x), NA, sign(x)*rnbinom.od(1, mu=abs(x), size=sqrt(1+abs(x)),d=scale.phi)))
+
+
+    processTriangle[is.na(processTriangle)] <- 0
+
+
+    IBNR <- processTriangle #apply(getLatest(gammaTriangle),3,sum)
+
+    output <- list(Triangle=Triangle, IBNR=IBNR, R=R, call=match.call(expand.dots = FALSE))
+    class(output) <- c("BootReserve", class(output))
+    return(output)
+
+}
+
+summary.BootReserve <- function(object,probs=c(0.75,0.99),...){
+
+    .Triangle <- object$Triangle
+    Latest <- rev(.Triangle[row(as.matrix(.Triangle)) == (nrow(.Triangle)+1 - col(as.matrix(.Triangle)))])
+    IBNR <- getLatest(object$IBNR)
+    dim(IBNR) <- dim(IBNR)[c(1,3)]
+    IBNR.q <- apply(IBNR, 1, quantile, probs)
+    IBNR.mean <- apply(IBNR, 1, mean)
+    IBNR.sd <- apply(IBNR, 1, sd)
+    sumIBNR <- as.data.frame(t(rbind(IBNR.mean, IBNR.sd, IBNR.q)))
+    result <- data.frame(Latest, Ult.mean=Latest+sumIBNR$IBNR.mean, sumIBNR)
+
+    names(result) <- c("Latest", "Mean Ultimate", "Mean IBNR", "SD IBNR", paste("IBNR ", probs*100, "%", sep=""))
+
+    return(result)
+}
+
+print.BootReserve <- function(x,probs=c(0.75,0.99),...){
+    print(x$call)
+    cat("\n")
+    res <- summary(x,probs=probs)
+
+    print(res, big.mark = ",", digits = 3,...)
+
+    Total.Latest <- sum(res$Latest,na.rm=TRUE)
+    Total.IBNR <- apply(getLatest(x$IBNR),3,sum)
+
+    Total.IBNR.mean <-  mean(Total.IBNR)
+    Total.IBNR.sd <-  sd(Total.IBNR)
+    Total.IBNR.q <- quantile(Total.IBNR, probs=probs)
+
+    Totals <-  c(Total.Latest, Total.Latest+Total.IBNR.mean, Total.IBNR.mean, Total.IBNR.sd, Total.IBNR.q)
+    Totals <- formatC(Totals, big.mark=",",digits=0,format="f")
+    Totals <- as.data.frame(Totals)
+
+    colnames(Totals)=c("Totals")
+    rownames(Totals) <- c("Latest:","Mean Ultimate:",
+                          "Mean IBNR:","SD IBNR:",
+                          paste("Total IBNR ", probs, "%:", sep="") )
+    cat("\n")
+    print(Totals, quote=FALSE)
+
+  }
+
+plot.BootReserve <- function(x,...){
+    IBNR <- getLatest(object$IBNR)
+    Total.IBNR <- apply(getLatest(x$IBNR),3,sum)
+    op <- par(mfrow=c(1,2))
+    hist(Total.IBNR, xlab="Total IBNR")
+    lines(density(Total.IBNR))
+    rug(Total.IBNR)
+
+    plot(ecdf(Total.IBNR), xlab="Total IBNR")
+    par(op)
+}
+
+
+makeCumulative <- function(tri){
+    ## Author: Nigel de Silva
+    tri <- apply(tri, c(1,3), cumsum)
+    tri <- aperm(tri, c(2,1,3))
+    return(tri)
+}
+
+getIncremental <- function(tri){
+    ## Author: Nigel de Silva
+    .incr <- function(x){
+        return(c(x[1], diff(x)))
+    }
+
+    out <- apply(tri, c(1,3), .incr)
+    out <- aperm(out, c(2,1,3))
+    return(out)
+}
+
+getLatest <- function(incr.tri){
+    ## Author: Nigel de Silva
+    out <- apply(incr.tri, c(1,3), sum, na.rm=T)
+    dim(out) <- c(1, dim(out))
+    out <- aperm(out, c(2,1,3))
+    return(out)
+}
+
+getIndivDFs <- function(tri){
+    ## Author: Nigel de Silva
+    .nDevPrds <- dim(tri)[2]
+    .numerator <- tri[ , c(2:.nDevPrds, .nDevPrds), , drop=F]
+    tri <- .numerator / tri
+    tri[ , .nDevPrds, ] <- NA
+    return(tri)
+}
+
+getAvDFs <- function(dfs, wghts){
+    ## Author: Nigel de Silva
+    .include <- dfs
+    .include[!is.na(.include)] <- 1
+    .include[is.na(wghts)] <- NA
+
+    out <- apply(dfs * wghts * .include, c(2,3), sum, na.rm=T) /
+      apply(wghts * .include, c(2,3), sum, na.rm=T)
+
+    out[is.nan(out)] <- 1
+
+
+  out <- array(out, c(1,dim(out)))
+  return(out)
+}
+
+getUltDFs <- function(avDFs){
+    ## Author: Nigel de Silva
+    .ultDF<- function(x){
+        return(rev(cumprod(rev(x))))
+    }
+
+    ult <- apply(avDFs, c(1,3), .ultDF)
+    ult <- aperm(ult, c(2,1,3))
+    return(ult)
+}
+
+getUltimates <- function(latest, ultDFs){
+    ## Author: Nigel de Silva
+    ultDFs <- apply(ultDFs, c(1,3), rev)
+    out <- latest * ultDFs
+    return(out)
+}
+
+expandArray <- function(x, d, new.size){
+    ## Author: Nigel de Silva
+    ## Expand array x, in d th dimension to new.size
+    ## Permute dimension to set changing dimension last
+  .n <- length(dim(x))
+  .perm <- 1:.n
+  .perm[d] <- .n
+  .perm[.n] <- d
+  x <- aperm(x, .perm)
+
+  ## Expand dimension
+  x <- array(x, dim=c(dim(x)[-.n], new.size))
+
+  ##Return to old dimension arrangement
+  x <- aperm(x, .perm)
+
+  return(x)
+
+}
+
+getExpected <- function(ults, ultDFs){
+    ## Author: Nigel de Silva
+    ## get backward chain ladder incremental triangles
+    ults <- expandArray(ults, 2, dim(ultDFs)[2])
+    ultDFs <- expandArray(ultDFs, 1, dim(ults)[1])
+
+    return(ults * ultDFs)
+}
+
+sampleResiduals <- function(resids, positions, n.sims){
+    ## Author: Nigel de Silva
+    ## Worry about excluding, zoning, etc. residuals later
+
+    resids <- as.vector(resids)
+    resids <- resids[!is.na(resids)]
+
+    .sample <- sample(resids, prod(dim(positions)[-3])*n.sims, replace=T)
+    .sample <- array(.sample, dim=c(dim(positions)[-3], n.sims))
+    .sample[is.na(positions)] <- NA
+
+    return(.sample)
+}
+
+randomClaims <- function(exp.clms, resids, n.sims){
+    ## Author: Nigel de Silva
+    .residSample <- sampleResiduals(resids, exp.clms, n.sims)
+    exp.clms <- expandArray(exp.clms, 3, n.sims)
+    out <- .residSample * sqrt(abs(exp.clms)) + exp.clms
+
+    return(out)
+}
+
+# Mike Lonergan mel at mcs.st-and.ac.uk
+# Fri Jun 14 19:31:24 CEST 2002
+# functions to generate random numbers following
+# over-dispersed Poisson and negative binomial distributions
+# based on simple cheat of using a standard negative binomial,
+# but choosing the scale parameter to give the desired mean/variance
+# ratio at the given value of the mean.
+
+rpois.od<-function (n, lambda,d=1) {
+    if (d==1)
+       rpois(n, lambda)
+    else
+       rnbinom(n, size=(lambda/(d-1)), mu=lambda)
+}
+
+rnbinom.od<-function (n, size, prob, mu, d=1) {
+    if (!missing(prob)) {
+       if (!missing(mu))
+          stop("prob and mu both specified")
+       mu<-size*(1-prob)/prob
+    }
+    size2 <- mu/(d-1+(d*mu/size))
+    prob2 <- size2/(size2 + mu)
+    .Internal(rnbinom(n, size2, prob2))
+}
---
+++ R/ChainLadder.R
@@ -0,0 +1,117 @@
+## Author: Markus Gesmann
+## Copyright: Markus Gesmann, markus.gesmann@gmail.com
+## Date:19/09/2008
+
+##
+## CL <- function(formula, weights=1/Triangle, Triangle){
+##     cl <- match.call()
+##     mf <- match.call(expand.dots = FALSE)
+##     m <- match("formula", names(mf), 0L)
+##     mf <- mf[c(m)]
+##     myModel <- vector("list", (n-1))
+##
+##     for(i in c(1:(n-1))){
+##         ## weighted linear regression through origin
+##         x <- Triangle[1:(m-i),i]
+##    	y <- Triangle[1:(m-i),i+1]
+##
+##   	myModel[[i]] <- lm(as.formula(mf), weights=weights[1:(m-i),i], data=data.frame(x,y))
+##     }
+##
+##     output <- list(Models=myModel, Triangle=Triangle)
+##     class(output) <- c("ChainLadder", "TriangleModel", class(output))
+##
+##  return(output)
+##
+## }
+
+
+
+ChainLadder <- function(Triangle, weights=1/Triangle){
+
+    n <- ncol(Triangle)
+    m <- nrow(Triangle)
+
+    if(n!=m){
+        print(dim(Triangle))
+        stop("Number of origin years has to be equal to number of development years.\n")
+    }
+    myModel <- vector("list", (n-1))
+    for(i in c(1:(n-1))){
+        ## weighted linear regression through origin
+        x <- Triangle[1:(m-i),i]
+   	y <- Triangle[1:(m-i),i+1]
+
+  	myModel[[i]] <- lm(y~x+0, weights=weights[1:(m-i),i], data=data.frame(x,y))
+    }
+
+    output <- list(Models=myModel, Triangle=Triangle)
+    class(output) <- c("ChainLadder", "TriangleModel", class(output))
+return(output)
+}
+
+print.ChainLadder <- function(x,...){
+cat("Chain-ladder factors:\n")
+print(sapply(x$Models, function(x) coef(x)))
+cat("Forecasted triangle:\n")
+print(predict(x))
+}
+
+###############################################################################
+## predict
+##
+predict.TriangleModel <- function(object,...){
+    n <- ncol(object[["Triangle"]])
+    m <- nrow(object[["Triangle"]])
+    FullTriangle <- object[["Triangle"]]
+    for(j in c(1:(n-1))){
+        FullTriangle[c((n-j+1):m), j+1] <- predict(object[["Models"]][[j]],
+                                                   newdata=data.frame(x=FullTriangle[c((n-j+1):m), j]),...)
+    }
+    return(FullTriangle)
+}
+
+################################################################################
+## estimate tail factor, idea from Thomas Mack:
+##       THE STANDARD ERROR OF CHAIN LADDER RESERVE ESTIMATES:
+##       RECURSIVE CALCULATION AND INCLUSION OF A TAIL FACTOR
+##
+tailfactor <- function (clratios){
+    f <- clratios
+    n <- length(f)
+    if (f[n - 2] * f[n - 1] > 1.0001) {
+        fn <- which(clratios > 1)
+        f <- clratios[fn]
+        n <- length(f)
+        tail.model <- lm(log(f - 1) ~ fn)
+        co <- coef(tail.model)
+        tail <- exp(co[1] + c(n:(n + 100)) * co[2]) + 1
+        tail <- prod(tail)
+        if (tail > 2){
+            print("The estimate tail factor was bigger than 2 and has been reset to 1.")
+            tail <- 1
+        }
+    }
+    else {
+        tail <- 1
+        tail.model <- NULL
+    }
+    return(list(tail.factor=tail, tail.model=tail.model))
+}
+
+
+checkTriangle <- function(Triangle){
+
+    .dim <- dim(Triangle)
+    n <- .dim[2]
+    m <- .dim[1]
+
+    if(length(.dim)==3 & .dim[3]==1){
+        dim(Triangle) <- c(m,n)
+    }
+    if(class(Triangle)=="data.frame"){
+        Triangle <- as.matrix(Triangle)
+    }
+
+    return(list(Triangle=Triangle, m=m,n=n))
+}
