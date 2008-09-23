2c02d3f70237c3d11f6da373f2bd32ddcfc68036
markus.gesmann <markus.gesmann@e5f87290-e53f-0410-917e-5545ed4be688>
2008-09-23 19:15:17 +0000

    added zzz.R and updated documentation
    git-svn-id: https://chainladder.googlecode.com/svn/trunk@71 e5f87290-e53f-0410-917e-5545ed4be688

---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+.onLoad<- function(lib, pkg, ...)
+{
+    cat("\n\n",
+        "ChainLadder library by Markus Gesmann <markus.gesmann@gmail.com>\n\n",
+        "Type library(help='ChainLadder') or ?ChainLadder\n",
+        "to see overall documentation.\n\n",
+        "Type example(ChainLadder) to get an idea of the functionality of this package.\n\n",
+        "If you would like to keep informed of new versions or have any feedback,\n",
+        "ideas or suggestion please drop me a line.\n\n",
+        sep='')
+  invisible()
+}
