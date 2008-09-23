d9484c384c2a1f5f6caf67fe06829050f770591b
markus.gesmann <markus.gesmann@e5f87290-e53f-0410-917e-5545ed4be688>
2008-09-23 22:15:09 +0000

    Updated documentation
    git-svn-id: https://chainladder.googlecode.com/svn/trunk@74 e5f87290-e53f-0410-917e-5545ed4be688

--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,9 @@
         "Type library(help='ChainLadder') or ?ChainLadder\n",
         "to see overall documentation.\n\n",
         "Type example(ChainLadder) to get an idea of the functionality of this package.\n\n",
+        "The ChainLadder project web site is: http://code.google.com/p/chainladder/\n\n",
         "If you would like to keep informed of new versions or have any feedback,\n",
-        "ideas or suggestion please drop me a line.\n\n",
+        "ideas or suggestion please drop me a line.\n",
         sep='')
   invisible()
 }
