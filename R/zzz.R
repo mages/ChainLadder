1b0293b739ae874c1a5e2b8efe6a2a0af2a2bc03
markus.gesmann <markus.gesmann@e5f87290-e53f-0410-917e-5545ed4be688>
2008-10-12 15:08:50 +0000

    Updated documentation on Mack-, Munich- and BootChainLaddder
    git-svn-id: https://chainladder.googlecode.com/svn/trunk@108 e5f87290-e53f-0410-917e-5545ed4be688

--- R/zzz.R
+++ R/zzz.R
@@ -5,9 +5,12 @@
         "Type library(help='ChainLadder') or ?ChainLadder\n",
         "to see overall documentation.\n\n",
         "Type example(ChainLadder) to get an idea of the functionality of this package.\n\n",
-        "The ChainLadder project web site is: http://code.google.com/p/chainladder/\n\n",
-        "If you would like to keep informed of new versions or have any feedback,\n",
-        "ideas or suggestion please drop me a line.\n\n",
+        "Feel free to send me an email if you would like to keep ",
+        "informed of\nnew versions or if you have any feedback, ",
+        "ideas, suggestions or ",
+        "would\nlike to collaborate.\n\n",
+        "More information is available on the ChainLadder project web-site:\n",
+        "http://code.google.com/p/chainladder/\n\n",
         sep='')
   invisible()
 }
