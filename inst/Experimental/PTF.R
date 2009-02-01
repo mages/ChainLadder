f3a03cd098847319136eebf1ec21a5138b610e3e
markus.gesmann <markus.gesmann@e5f87290-e53f-0410-917e-5545ed4be688>
2009-02-01 16:21:40 +0000

    Changed PTF to reflect example data
    git-svn-id: https://chainladder.googlecode.com/svn/trunk@148 e5f87290-e53f-0410-917e-5545ed4be688

--- inst/Experimental/PTF.R
+++ inst/Experimental/PTF.R
@@ -5,6 +5,7 @@ origin <- 1978 : 1991
 dev <- 0:13
 
 dev.trend <- matrix(-0.2, nrow=14,ncol=14)
+dev.trend[,1] <- 0
 dev.trend[row(dev.trend) > 15-col(dev.trend)] <- NA
 dimnames(dev.trend) <-  list(origin=origin, dev=dev)
 
@@ -14,15 +15,16 @@ dimnames(cal.trend) <-  list(origin=origin, dev=dev)
 cal.trend[row(cal.trend) > 15-col(cal.trend)] <- NA
 cal.trend[row(cal.trend)+col(cal.trend)<=6] <- 0.1
 cal.trend[row(cal.trend)+col(cal.trend)==7] <- 0.3
+cal.trend[1,1] <- 0
 ### Cumulative trends
 cum.dev.trend <- t(apply(dev.trend, 1, cumsum))
 cum.cal.trend <- cal.trend
 for(i in 1:5){
-    cum.cal.trend[row(cal.trend)+col(cal.trend)==i+1] <- 0.1*i
+    cum.cal.trend[row(cal.trend)+col(cal.trend)==i+1] <- 0.1*(i-1)
 }
-cum.cal.trend[row(cal.trend)+col(cal.trend)==7] <- 0.1*5+0.3
+cum.cal.trend[row(cal.trend)+col(cal.trend)==7] <- 0.1*4+0.3
 for(i in 7:14){
-    cum.cal.trend[row(cal.trend)+col(cal.trend)==i+1] <- 0.1*5+0.3+0.15*(i-6)
+    cum.cal.trend[row(cal.trend)+col(cal.trend)==i+1] <- 0.1*4+0.3+0.15*(i-6)
 }
 
 y <- expand.grid( list(origin=origin, dev=dev))
@@ -34,6 +36,7 @@ y$cum.cal.trend <- as.vector(cum.cal.trend)
 y$paid <- 11.51293 + with(y, cum.dev.trend +  cum.cal.trend)
 y <- na.omit(y)
 
+
 library(lattice)
 xyplot(paid ~ dev , groups=origin, data=subset(y, origin<1984), t="l",
        main="Figure 15\nPlot of log(Paid) data against delay\nfor the first six accident years")
@@ -41,14 +44,14 @@ xyplot(paid ~ dev , groups=origin, data=subset(y, origin<1984), t="l",
 
 ## Random noise
 ## page 270
-set.seed(1)
+set.seed(3)
 y$paid.noise <- y$paid + rnorm(nrow(y), 0, 0.1)
 
 xyplot(paid.noise ~ dev , groups=origin, data=subset(y, origin<1984), t="l",
        main="Figure 16\n Trend plus randomness for the first six accident years")
 
 
-model <- lm(paid.noise ~  dev + I(cal-1977), data=y)
+model <- lm(paid.noise ~  dev + cal, data=y)
 ## Table 3.1 page 274
 summary(model)
 op <- par(mfrow=c(2,2))
@@ -75,10 +78,15 @@ par(op)
  pairs(y[c("paid.noise", "dev", "cal")], panel=panel.smooth)
 ### Add payment year trends to the model
 
-y$cal.cut <- cut(y$cal, c(1977, 1982, 1983, 1991))
+y$cal.cut <- cut(y$cal, c(1977, 1978, 1981, 1983, 1991))
 model.cal.trends <- lm(paid.noise ~  dev + I(cal-1977) + cal.cut, data=y)
 ## Table 3.2 page 277
 summary(model.cal.trends)
 op <- par(mfrow=c(2,2))
 plot(model.cal.trends)
 par(op)
+
+x <- c(1:10)
+y <- c(1:5, 12, 14, 16, 18, 20)
+
+inf <- factor(c(rep(1,5), 2, rep(3,4)))
