bd5538efd982a469ae30550461255fcde11e0e9b
markus.gesmann <markus.gesmann@e5f87290-e53f-0410-917e-5545ed4be688>
2010-01-10 20:18:12 +0000

    Added test for mcl to check for dimension of triangles. Increased version number to 0.1.3-1
    git-svn-id: https://chainladder.googlecode.com/svn/trunk@215 e5f87290-e53f-0410-917e-5545ed4be688

--- R/multiChainLadder.R
+++ R/multiChainLadder.R
@@ -2,35 +2,40 @@
 ###################################################################################
 # Main function to fit model, predict, and calculate mse under MCL
 
-mcl=function(triangles,
-			fit.method="SUR", 
-			delta=1,
-			extrap="Mack",
-			mse.method="Mack",...){
+mcl <- function(triangles,
+                fit.method="SUR",
+                delta=1,
+                extrap="Mack",
+                mse.method="Mack",...){
 
-	p=length(triangles)
-	m=dim(triangles[[1]])[1]
-	n=dim(triangles[[1]])[2]
-	
-	# fit the mcl model 
-	models=fit.mcl(triangles=triangles,
-				fit.method=fit.method,
-				delta=delta,
-				extrap=extrap,...)
-	
-	# fill the triangle
-	fullTriangles=predict.mclmodels(models)	 
-	
-	# calculate the mse 
-	mse=mse.mcl(mse.method=mse.method,
-				models=models,
-				fullTriangles=fullTriangles)
+    ## Check if the dimensions of all triangles are equal
+    if(!check.triangles.dimensions(triangles)){
+        stop("Please ensure all triangles have the same dimensions.\n")
+    }
+
+    p <- length(triangles)
+    m <- dim(triangles[[1]])[1]
+    n <- dim(triangles[[1]])[2]
+
+    ## fit the mcl model
+    models <- fit.mcl(triangles=triangles,
+                      fit.method =fit.method,
+                      delta=delta,
+                      extrap=extrap,...)
+
+    ## fill the triangle
+    fullTriangles <- predict.mclmodels(models)
+
+    ## calculate the mse
+    mse <- mse.mcl(mse.method=mse.method,
+                   models=models,
+                   fullTriangles=fullTriangles)
 
 
-	output=c(models,mse)
-	
-	class(output)=c("gmcl","list")     
-	return(output)
+    output <- c(models,mse)
+
+    class(output) <- c("gmcl","list")
+    return(output)
 }
 
 ######################################################################################
@@ -38,7 +43,7 @@ mcl=function(triangles,
 
 fit.mcl=function(triangles,
 			fit.method="SUR",
-			delta=1, 
+			delta=1,
 			extrap="Mack",...)
 	{
 	p=length(triangles)
@@ -46,27 +51,27 @@ fit.mcl=function(triangles,
 	n=dim(triangles[[1]])[2]
 
 	B=Bcov=ecov=myModel=vector("list",n-1)
-	 
-		for (i in 1:(n-1)){   			
+
+		for (i in 1:(n-1)){
 			y=x=system=vector("list",p)
 			for (j in 1:p){
-				y[[j]]=triangles[[j]][1:(m-i),i+1]*(triangles[[j]][1:(m-i),i]^(-delta/2))   
-				x[[j]]=triangles[[j]][1:(m-i),i]*(triangles[[j]][1:(m-i),i]^(-delta/2)) 
+				y[[j]]=triangles[[j]][1:(m-i),i+1]*(triangles[[j]][1:(m-i),i]^(-delta/2))
+				x[[j]]=triangles[[j]][1:(m-i),i]*(triangles[[j]][1:(m-i),i]^(-delta/2))
 				system[[j]]=as.formula(paste("y[[",j,"]]~-1+x[[",j,"]]",sep=""))
-								
+
 			}
 			if (!(i==n-1 & extrap!=FALSE )){
-				myModel[[i]]=systemfit(system,fit.method,...)  
+				myModel[[i]]=systemfit(system,fit.method,...)
 				B[[i]]= as.vector(coef(myModel[[i]]))
 				Bcov[[i]]=unname(vcov(myModel[[i]]))
 				ecov[[i]]=unname(myModel[[i]]$residCov)
-				if (fit.method %in% "OLS") ecov[[i]]=diag(diag(ecov[[i]]),nrow=p) 
+				if (fit.method %in% "OLS") ecov[[i]]=diag(diag(ecov[[i]]),nrow=p)
 				# replace off-diagonal elements as 0
 			}
 			else {
 				B[[i]]=sapply(1:p,function(x) triangles[[x]][1,i+1]/triangles[[x]][1,i])
 				if (extrap %in% "Genvar"){
-					ecov[[i]]=(det(ecov[[i-1]])^2/det(ecov[[i-2]]))^(1/p)*ecov[[i-1]]								
+					ecov[[i]]=(det(ecov[[i-1]])^2/det(ecov[[i-2]]))^(1/p)*ecov[[i-1]]
 				}
 				if (extrap %in% "Mack"){
 					r=abs(ecov[[i-1]]^2/ecov[[i-2]])
@@ -74,7 +79,7 @@ fit.mcl=function(triangles,
 				}
 				Bcov[[i]]=solve(diag(unlist(x),nrow=p)%*%solve(ecov[[i]])%*%diag(unlist(x),nrow=p))
 				myModel[[i]]="Not applicable because of extrapolation"
-			}	
+			}
 		}
 	output=list()
 	output[["triangles"]]=triangles
@@ -101,7 +106,7 @@ predict.mclmodels=function(object,...)
 	n=dim(triangles[[1]])[2]
 	fullTriangles=triangles
     	for (i in 1:(n-1)){
-        	x=sapply(1:p, function(x) fullTriangles[[x]][(m-i+1):m,i])  
+        	x=sapply(1:p, function(x) fullTriangles[[x]][(m-i+1):m,i])
         	x=matrix(x,p,i,byrow=TRUE)
 			y=diag(B[[i]],nrow=p)%*%x
 			for (j in 1:p){
@@ -121,16 +126,16 @@ mse.mcl=function(mse.method="Mack", models, fullTriangles)
 	triangles=models$triangles
 	p=length(fullTriangles)
 	n=ncol(fullTriangles[[1]])
-	m=nrow(fullTriangles[[1]])  	
+	m=nrow(fullTriangles[[1]])
 	B=models$B
 	Bcov=models$Bcov
 	ecov=models$ecov
 	delta=models$delta
-	
+
 	mse.ay=mse.ay.est=mse.ay.proc=matrix(0,m*p,n*p)
 	mse.total=mse.total.est=mse.total.proc=matrix(0,p,p*n)
-	
-	
+
+
 	# mse for single accident years
 	for ( k in 1:(n-1))
 		{
@@ -140,13 +145,13 @@ mse.mcl=function(mse.method="Mack", models, fullTriangles)
 			b1=(p*(k-1)+1):(p*k)
 			a2=(p*(i-1)+1):(p*i)	# new indexes
 			b2=(p*k+1):(p*(k+1))
-			
+
 			yhat=sapply(1:p,function(x) fullTriangles[[x]][i,k])
 			#process variance
 			mse.ay.proc[a2,b2]=diag(yhat^(delta/2),nrow=p)%*%ecov[[k]]%*%diag(yhat^(delta/2),nrow=p)+
 								B[[k]]%*%t(B[[k]])*mse.ay.proc[a1,b1]
-			
-			#estimation variance	
+
+			#estimation variance
 			if (mse.method %in% "Mack")	#Mack formulas
 				{
 				mse.ay.est[a2,b2]=Bcov[[k]]*(yhat%*%t(yhat))+
@@ -157,30 +162,30 @@ mse.mcl=function(mse.method="Mack", models, fullTriangles)
 				mse.ay.est[a2,b2]=Bcov[[k]]*(yhat%*%t(yhat))+
 									(B[[k]]%*%t(B[[k]]))*mse.ay.est[a1,b1]+
 									Bcov[[k]]*mse.ay.est[a1,b1]
-				}											
+				}
 			#mse
-			mse.ay[a2,b2]=mse.ay.proc[a2,b2]+mse.ay.est[a2,b2]					
+			mse.ay[a2,b2]=mse.ay.proc[a2,b2]+mse.ay.est[a2,b2]
 			}
 		}
-	
-	# mse for aggregated accident years	
+
+	# mse for aggregated accident years
 	for (k in 1:(n-1))
 		{
-		
-		b1=(p*(k-1)+1):(p*k) 
+
+		b1=(p*(k-1)+1):(p*k)
 		b2=(p*k+1):(p*(k+1))
 		proc=matrix(0,p,p)
-		
+
 		#process variance
 		for (i in ((m+1-k):m))
-			{			
+			{
 			yhat=sapply(1:p,function(x) fullTriangles[[x]][i,k])
 			proc=proc+diag(yhat^(delta/2),nrow=p)%*%ecov[[k]]%*%diag(yhat^(delta/2),nrow=p)
-			}	
-			
+			}
+
 		sumyhat=sapply(1:p,function(x) sum(fullTriangles[[x]][(m+1-k):m,k]))
 		mse.total.proc[,b2]=proc+B[[k]]%*%t(B[[k]])*mse.total.proc[,b1]
-		
+
 		#estimation variance
 		if (mse.method == "Mack")
 			{
@@ -192,13 +197,13 @@ mse.mcl=function(mse.method="Mack", models, fullTriangles)
 			mse.total.est[,b2]=Bcov[[k]]*(sumyhat%*%t(sumyhat))+
 									(B[[k]]%*%t(B[[k]]))*mse.total.est[,b1]+
 									Bcov[[k]]*mse.total.est[,b1]
-			}		
-		
+			}
+
 		#total variance
 		mse.total[,b2]=mse.total.proc[,b2]+mse.total.est[,b2]
 		}
-		
-				
+
+
 	return(list(mse.ay=mse.ay,
 				mse.ay.est=mse.ay.est,
 				mse.ay.proc=mse.ay.proc,
@@ -209,5 +214,8 @@ mse.mcl=function(mse.method="Mack", models, fullTriangles)
 				mse.method=mse.method))
 	}
 
-
-
+check.triangles.dimensions <- function(triangles){
+## Check if the dimensions of all triangles are equal
+    dims <- sapply(triangles, dim)
+    all( dims - apply(dims, 1, mean) ==0)
+}
