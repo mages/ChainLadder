5714295ca731138d796000384b785ec4a1274670
actuaryzhang10 <actuaryzhang10@e5f87290-e53f-0410-917e-5545ed4be688>
2010-02-12 00:38:09 +0000

    git-svn-id: https://chainladder.googlecode.com/svn/trunk@222 e5f87290-e53f-0410-917e-5545ed4be688

--- R/gmultiChainLadder.R
+++ R/gmultiChainLadder.R
@@ -6,77 +6,87 @@
 
 
 
-gmcl=function(triangles,fit.method="SUR", delta=1,...){
+GeneralMultiChainLadder <- function(triangles,fit.method="SUR", delta=1,...){
 
-	p=length(triangles)
-	m=dim(triangles[[1]])[1]
-	n=dim(triangles[[1]])[2]
+    ## Check if the dimensions of all triangles are equal
+    if(!check.triangles.dimensions(triangles)){
+        stop("Please ensure all triangles have the same dimensions.\n")
+    }
+
+	p <- length(triangles)
+	m <- dim(triangles[[1]])[1]
+	n <- dim(triangles[[1]])[2]
 	
 	# fit regression models
-	models=fit.gmcl(triangles=triangles,fit.method=fit.method,delta=delta,...)
+	models <- fit.GeneralMultiChainLadder(triangles=triangles,
+						fit.method=fit.method,
+						delta=delta,...)
 	
 	# complete triangles
-	fullTriangles=predict.gmclmodels(models)	 	
+	fullTriangles <- predict(models)	 	
 	
 	# calculate mse
-	mse=mse.gmcl(models=models,fullTriangles=fullTriangles)
+	mse <- mse.GeneralMultiChainLadder(models=models,
+						fullTriangles=fullTriangles)
 	
-	output=c(models,mse)
+	output <- c(models,mse)
 	
-	class(output)=c("gmcl","list")    
+	class(output) <- c("GeneralMultiChainLadder","list")    
 	return(output)
 }
 
-fit.gmcl=function(triangles,fit.method="SUR",delta=1, ...)
+fit.GeneralMultiChainLadder <- function(triangles,
+						fit.method="SUR",
+						delta=1, ...)
 	{
-	p=length(triangles)
-	m=dim(triangles[[1]])[1]
-	n=dim(triangles[[1]])[2]
-	myModel=vector("list",n-1)  #########this is a list with all the fitted regressions
-	B=Bcov=ecov=vector("list",n-1)
-	chngOrder=as.vector(matrix(1:(p^2),p,p,byrow=TRUE))    # Change the order due to the vectorization  
+	p <- length(triangles)
+	m <- dim(triangles[[1]])[1]
+	n <- dim(triangles[[1]])[2]
+	myModel <- vector("list",n-1)  #########this is a list with all the fitted regressions
+	B <- Bcov <- ecov <- vector("list",n-1)
+	chngOrder <- as.vector(matrix(1:(p^2),p,p,byrow=TRUE))    # Change the order due to the vectorization  
 		for (i in 1:(n-1)){   			
-			y=x=system=vector("list",p)
+			y <- x <- system <- vector("list",p)
 			for (j in 1:p){
-				y[[j]]=triangles[[j]][1:(m-i),i+1]*(triangles[[j]][1:(m-i),i])^(-delta/2)   
-				x[[j]]=lapply(1:p,function(x) triangles[[x]][1:(m-i),i]*(triangles[[j]][1:(m-i),i])^(-delta/2)) 
+				y[[j]] <- triangles[[j]][1:(m-i),i+1]*(triangles[[j]][1:(m-i),i])^(-delta/2)   
+				x[[j]] <- lapply(1:p,function(x) triangles[[x]][1:(m-i),i]*(triangles[[j]][1:(m-i),i])^(-delta/2)) 
 				
-				system[[j]]=as.formula(
+				system[[j]] <- as.formula(
 							paste("y[[",j,"]]~-1+",   
 								paste("x[[",j,"]][[",1:p,"]]",sep="",collapse="+"),sep="")) 
 			}
 			
-			myModel[[i]]=systemfit(system,fit.method,...)  
-			B[[i]]= matrix(coef(myModel[[i]]),p,p,byrow=TRUE)  
-			Bcov[[i]]=vcov(myModel[[i]])[chngOrder,chngOrder] 
-			ecov[[i]]=myModel[[i]]$residCov  
+			myModel[[i]] <- systemfit(system,fit.method,...)  
+			B[[i]] <-  matrix(coef(myModel[[i]]),p,p,byrow=TRUE)  
+			Bcov[[i]] <- vcov(myModel[[i]])[chngOrder,chngOrder] 
+			ecov[[i]] <- myModel[[i]]$residCov  
 		}
-	output=list()
-	output[["triangles"]]=triangles
-	output[["models"]]=myModel
-	output[["B"]]=B
-	output[["Bcov"]]=Bcov
-	output[["ecov"]]=ecov
-	output[["fit.method"]]=fit.method
-	output[["delta"]]=delta
-	class(output)="gmclmodels"
+	output <- list()
+	output[["triangles"]] <- triangles
+	output[["models"]] <- myModel
+	output[["B"]] <- B
+	output[["Bcov"]] <- Bcov
+	output[["ecov"]] <- ecov
+	output[["fit.method"]] <- fit.method
+	output[["delta"]] <- delta
+	class(output) <- "GeneralMultiChainLadderFit"
 	return(output)
 }
 
-predict.gmclmodels=function(object,...)
+predict.GeneralMultiChainLadderFit <- function(object,...)
 	{
-	triangles=object$triangles
-	B=object$B
-	p=length(triangles)
-	m=dim(triangles[[1]])[1]
-	n=dim(triangles[[1]])[2]
-	fullTriangles=triangles
+	triangles <- object$triangles
+	B <- object$B
+	p <- length(triangles)
+	m <- dim(triangles[[1]])[1]
+	n <- dim(triangles[[1]])[2]
+	fullTriangles <- triangles
     	for (i in 1:(n-1)){
-        	x= sapply(1:p, function(x) fullTriangles[[x]][(m-i+1):m,i])  
-        	x=matrix(as.vector(x),p,i,byrow=TRUE)
-			y=B[[i]]%*%x
+        	x <-  sapply(1:p, function(x) fullTriangles[[x]][(m-i+1):m,i])  
+        	x <- matrix(as.vector(x),p,i,byrow=TRUE)
+			y <- B[[i]]%*%x
 			for (j in 1:p){
-				fullTriangles[[j]][(m-i+1):m,(i+1)]=y[j,]
+				fullTriangles[[j]][(m-i+1):m,(i+1)] <- y[j,]
 			}
 		}
 	return(fullTriangles)
@@ -85,42 +95,42 @@ predict.gmclmodels=function(object,...)
 
 #####################Functions to compute the MSE in the  gmcl model#####################
 
-mse.gmcl=function(models, fullTriangles){
+mse.GeneralMultiChainLadder <- function(models, fullTriangles){
 
-	triangles=models$triangles
-	p=length(fullTriangles)
-	n=ncol(fullTriangles[[1]])
-	m=nrow(fullTriangles[[1]])
-   	nAdd=sum(!is.na(triangles[[1]][,n]))-1   	#####number of rows in addition to the traditional triangle
-	Ip=diag(rep(1,p))
-	B=models$B
-	Bcov=models$Bcov
-	ecov=models$ecov
-	delta=models$delta
+	triangles <- models$triangles
+	p <- length(fullTriangles)
+	n <- ncol(fullTriangles[[1]])
+	m <- nrow(fullTriangles[[1]])
+   	nAdd <- sum(!is.na(triangles[[1]][,n]))-1   	#####number of rows in addition to the traditional triangle
+	Ip <- diag(rep(1,p))
+	B <- models$B
+	Bcov <- models$Bcov
+	ecov <- models$ecov
+	delta <- models$delta
 	
-	mse.ay=mse.ay.est=mse.ay.proc=matrix(0,m*p,n*p)
-	mse.total=mse.total.est=mse.total.proc=matrix(0,p,p*n)
+	mse.ay <- mse.ay.est <- mse.ay.proc <- matrix(0,m*p,n*p)
+	mse.total <- mse.total.est <- mse.total.proc <- matrix(0,p,p*n)
 	
 	# recursive calcualtion of mse for single accident years
 	for ( i in (2+nAdd):m)
 		{
 		for (k in (n+nAdd+1-i):(n-1))
 			{
-			yhat=sapply(1:p,function(x) fullTriangles[[x]][i,k])
-			a1=(p*(i-1)+1):(p*i)   # old indexes
-			b1=(p*(k-1)+1):(p*k)
-			a2=(p*(i-1)+1):(p*i)		# new indexes
-			b2=(p*k+1):(p*(k+1))
+			yhat <- sapply(1:p,function(x) fullTriangles[[x]][i,k])
+			a1 <- (p*(i-1)+1):(p*i)   # old indexes
+			b1 <- (p*(k-1)+1):(p*k)
+			a2 <- (p*(i-1)+1):(p*i)		# new indexes
+			b2 <- (p*k+1):(p*(k+1))
 			
 			# process variance
-			mse.ay.proc[a2,b2]=B[[k]]%*%mse.ay.proc[a1,b1]%*%t(B[[k]])+
+			mse.ay.proc[a2,b2] <- B[[k]]%*%mse.ay.proc[a1,b1]%*%t(B[[k]])+
 								diag((yhat)^(delta/2),nrow=p)%*%ecov[[k]]%*%diag((yhat)^(delta/2),nrow=p)
 			# estimation variance				
-			mse.ay.est[a2,b2]=B[[k]]%*%mse.ay.est[a1,b1]%*%t(B[[k]])+
+			mse.ay.est[a2,b2] <- B[[k]]%*%mse.ay.est[a1,b1]%*%t(B[[k]])+
 								kronecker(t(yhat),Ip)%*%Bcov[[k]]%*%kronecker(yhat,Ip)
  
  			# combined
-			mse.ay[a2,b2]=mse.ay.proc[a2,b2]+mse.ay.est[a2,b2]					
+			mse.ay[a2,b2] <- mse.ay.proc[a2,b2]+mse.ay.est[a2,b2]					
 			}
 		}
 		
@@ -128,25 +138,25 @@ mse.gmcl=function(models, fullTriangles){
 	for (k in 1:(n-1))
 		{
 			
-		sumProc=matrix(0,p,p)
-		sumyhat=rep(0,p)
+		sumProc <- matrix(0,p,p)
+		sumyhat <- rep(0,p)
 		for (i in ((m+1-k):m))
 			{
-			yhat=sapply(1:p,function(x) fullTriangles[[x]][i,k])
+			yhat <- sapply(1:p,function(x) fullTriangles[[x]][i,k])
 			
-			sumProc=sumProc+diag((yhat)^(delta/2),nrow=p)%*%ecov[[k]]%*%diag((yhat)^(delta/2),nrow=p)			
-			sumyhat=sumyhat+yhat	
+			sumProc <- sumProc+diag((yhat)^(delta/2),nrow=p)%*%ecov[[k]]%*%diag((yhat)^(delta/2),nrow=p)			
+			sumyhat <- sumyhat+yhat	
 			}
 			
-		b1=(p*(k-1)+1):(p*k)   #old index
-		b2=(p*k+1):(p*(k+1))   #new index
+		b1 <- (p*(k-1)+1):(p*k)   #old index
+		b2 <- (p*k+1):(p*(k+1))   #new index
 		
-		mse.total.proc[,b2]=B[[k]]%*%mse.total.proc[,b1]%*%t(B[[k]])+sumProc
+		mse.total.proc[,b2] <- B[[k]]%*%mse.total.proc[,b1]%*%t(B[[k]])+sumProc
 		
-		mse.total.est[,b2]=B[[k]]%*%mse.total.est[,b1]%*%t(B[[k]])+
+		mse.total.est[,b2] <- B[[k]]%*%mse.total.est[,b1]%*%t(B[[k]])+
 							kronecker(t(sumyhat),Ip)%*%Bcov[[k]]%*%kronecker(sumyhat,Ip)
 							
-		mse.total[,b2]=mse.total.proc[,b2]+mse.total.est[,b2]
+		mse.total[,b2] <- mse.total.proc[,b2]+mse.total.est[,b2]
 		}
 	return(list(mse.ay=mse.ay,
 				mse.ay.est=mse.ay.est,
@@ -160,81 +170,81 @@ mse.gmcl=function(models, fullTriangles){
 
 ##### summarize the gmcl results
 
-summary.gmcl=function(object,agg=NULL,...){
+summary.GeneralMultiChainLadder <- function(object,agg=NULL,...){
 	
-	p=length(object$triangles)
-	m=dim(object$triangles[[1]])[1]
-	n=dim(object$triangles[[1]])[2]
-	nAdd=sum(!is.na(object$triangles[[1]][,n]))-1
+	p <- length(object$triangles)
+	m <- dim(object$triangles[[1]])[1]
+	n <- dim(object$triangles[[1]])[2]
+	nAdd <- sum(!is.na(object$triangles[[1]][,n]))-1
 
-	if (is.null(agg)) agg=1:p else agg=as.numeric(unlist(strsplit(agg,"\\+",perl=TRUE)))
+	if (is.null(agg)) agg <- 1:p else agg <- as.numeric(unlist(strsplit(agg,"\\+",perl=TRUE)))
 	
-	Latest=lapply(1:p, function(x) {
+	Latest <- lapply(1:p, function(x) {
 		c(rev(object[["triangles"]][[x]]
 		[row(as.matrix(object[["triangles"]][[x]]))
 		==(m+1-col(as.matrix(object[["triangles"]][[x]])))])[-1])})
-	Latest=lapply(1:p, function(x){
+	Latest <- lapply(1:p, function(x){
 		c(Latest[[x]],sum(Latest[[x]]))
 		})
 	
-	Ultimate=lapply(1:p, function(x){
+	Ultimate <- lapply(1:p, function(x){
 		c(object$fullTriangles[[x]][-(1:(nAdd+1)),n])
 		})
-	Ultimate=lapply(1:p, function(x){
+	Ultimate <- lapply(1:p, function(x){
 		c(Ultimate[[x]],sum(Ultimate[[x]]))
 		})	
 
-	s=(nAdd+1)*p
+	s <- (nAdd+1)*p
 	
-	mse.ay.ult=object$mse.ay[-(1:s),((n-1)*p+1):(p*n)]
-	mse.ay.ult.est=object$mse.ay.est[-(1:s),((n-1)*p+1):(p*n)]
-	mse.ay.ult.proc=object$mse.ay.proc[-(1:s),((n-1)*p+1):(p*n)]
-	mse.total.ult=object$mse.total[,((n-1)*p+1):(p*n)]
-	mse.total.ult.est=object$mse.total.est[,((n-1)*p+1):(p*n)]
-	mse.total.ult.proc=object$mse.total.proc[,((n-1)*p+1):(p*n)]
+	mse.ay.ult <- object$mse.ay[-(1:s),((n-1)*p+1):(p*n)]
+	mse.ay.ult.est <- object$mse.ay.est[-(1:s),((n-1)*p+1):(p*n)]
+	mse.ay.ult.proc <- object$mse.ay.proc[-(1:s),((n-1)*p+1):(p*n)]
+	mse.total.ult <- object$mse.total[,((n-1)*p+1):(p*n)]
+	mse.total.ult.est <- object$mse.total.est[,((n-1)*p+1):(p*n)]
+	mse.total.ult.proc <- object$mse.total.proc[,((n-1)*p+1):(p*n)]
 	
 	if (p==1){
-		Latest=unlist(Latest)
-		Ultimate=unlist(Ultimate)
-		IBNR=Ultimate-Latest
-		Dev.To.Date= Latest/Ultimate
-		mse.ult=c(mse.ay.ult,mse.total.ult)
-		mse.ult.est=c(mse.ay.ult.est,mse.total.ult.est)
-		mse.ult.proc=c(mse.ay.ult.proc,mse.total.ult.proc)
-		S.E=round(sqrt(mse.ult),2)
-		S.E.Est=round(sqrt(mse.ult.est),2)
-		S.E.Proc=round(sqrt(mse.ult.proc),2)	
-		CV=S.E/IBNR
-		output=data.frame(Latest, Dev.To.Date, Ultimate, IBNR, S.E, CV, S.E.Est, S.E.Proc)
-		rownames(output)=c(as.character((nAdd+2):m),"Total")
+		Latest <- unlist(Latest)
+		Ultimate <- unlist(Ultimate)
+		IBNR <- Ultimate-Latest
+		Dev.To.Date <-  Latest/Ultimate
+		mse.ult <- c(mse.ay.ult,mse.total.ult)
+		mse.ult.est <- c(mse.ay.ult.est,mse.total.ult.est)
+		mse.ult.proc <- c(mse.ay.ult.proc,mse.total.ult.proc)
+		S.E <- round(sqrt(mse.ult),2)
+		S.E.Est <- round(sqrt(mse.ult.est),2)
+		S.E.Proc <- round(sqrt(mse.ult.proc),2)	
+		CV <- S.E/IBNR
+		output <- data.frame(Latest, Dev.To.Date, Ultimate, IBNR, S.E, CV, S.E.Est, S.E.Proc)
+		rownames(output) <- c(as.character((nAdd+2):m),"Total")
 		}	
 
 	else {
-		Latest[[p+1]]=Reduce("+",Latest[agg])
-		Ultimate[[p+1]]=Reduce("+",Ultimate[agg])
-		IBNR=lapply(1:(p+1),function(x) Ultimate[[x]]-Latest[[x]])
-		Dev.To.Date=lapply(1:(p+1),function(x) Latest[[x]]/Ultimate[[x]])
-		mse.ult.matrix=rbind(mse.ay.ult,mse.total.ult)
-		mse.ult=lapply(1:p, function(x) mse.ult.matrix[((1:(dim(mse.ult.matrix)[1]/p)-1)*p+x),x])
-		mse.ult[[p+1]]=sapply(1:(dim(mse.ult.matrix)[1]/p), 
+		Latest[[p+1]] <- Reduce("+",Latest[agg])
+		Ultimate[[p+1]] <- Reduce("+",Ultimate[agg])
+		IBNR <- lapply(1:(p+1),function(x) Ultimate[[x]]-Latest[[x]])
+		Dev.To.Date <- lapply(1:(p+1),function(x) Latest[[x]]/Ultimate[[x]])
+		mse.ult.matrix <- rbind(mse.ay.ult,mse.total.ult)
+		mse.ult <- lapply(1:p, function(x) mse.ult.matrix[((1:(dim(mse.ult.matrix)[1]/p)-1)*p+x),x])
+		mse.ult[[p+1]] <- sapply(1:(dim(mse.ult.matrix)[1]/p), 
 							function(x) sum(mse.ult.matrix[(agg+(x-1)*p),agg]))
 							
-		mse.ult.est.matrix=rbind(mse.ay.ult.est,mse.total.ult.est)
-		mse.ult.est=lapply(1:p, function(x) mse.ult.est.matrix[((1:(dim(mse.ult.est.matrix)[1]/p)-1)*p+x),x])
-		mse.ult.est[[p+1]]=sapply(1:(dim(mse.ult.est.matrix)[1]/p), 
+		mse.ult.est.matrix <- rbind(mse.ay.ult.est,mse.total.ult.est)
+		mse.ult.est <- lapply(1:p, function(x) mse.ult.est.matrix[((1:(dim(mse.ult.est.matrix)[1]/p)-1)*p+x),x])
+		mse.ult.est[[p+1]] <- sapply(1:(dim(mse.ult.est.matrix)[1]/p), 
 							function(x) sum(mse.ult.est.matrix[(agg+(x-1)*p),agg]))
 							
-		mse.ult.proc.matrix=rbind(mse.ay.ult.proc,mse.total.ult.proc)
-		mse.ult.proc=lapply(1:p, function(x) mse.ult.proc.matrix[((1:(dim(mse.ult.proc.matrix)[1]/p)-1)*p+x),x])
-		mse.ult.proc[[p+1]]=sapply(1:(dim(mse.ult.proc.matrix)[1]/p), 
+		mse.ult.proc.matrix <- rbind(mse.ay.ult.proc,mse.total.ult.proc)
+		mse.ult.proc <- lapply(1:p, function(x) mse.ult.proc.matrix[((1:(dim(mse.ult.proc.matrix)[1]/p)-1)*p+x),x])
+		mse.ult.proc[[p+1]] <- sapply(1:(dim(mse.ult.proc.matrix)[1]/p), 
 							function(x) sum(mse.ult.proc.matrix[(agg+(x-1)*p),agg]))														
-		S.E=lapply(1:(p+1), function(x) sqrt(mse.ult[[x]]))
-		S.E.Est=lapply(1:(p+1), function(x) sqrt(mse.ult.est[[x]]))
-		S.E.Proc=lapply(1:(p+1), function(x) sqrt(mse.ult.proc[[x]]))
+		S.E <- lapply(1:(p+1), function(x) sqrt(mse.ult[[x]]))
+		S.E.Est <- lapply(1:(p+1), function(x) sqrt(mse.ult.est[[x]]))
+		S.E.Proc <- lapply(1:(p+1), function(x) sqrt(mse.ult.proc[[x]]))
 		
-		CV=lapply(1:(p+1), function(x) S.E[[x]]/IBNR[[x]])
-		output=lapply(1:(p+1), function(x) {
-						output=data.frame(Latest=Latest[[x]], 
+		CV <- lapply(1:(p+1), function(x) S.E[[x]]/IBNR[[x]])
+		output <- lapply(1:(p+1), function(x) {
+						output <- data.frame(Latest=Latest[[x]], 
 								Dev.To.Date=round(Dev.To.Date[[x]],4), 
 								Ultimate=Ultimate[[x]], 
 								IBNR=IBNR[[x]], 
@@ -242,11 +252,11 @@ summary.gmcl=function(object,agg=NULL,...){
 								CV=round(CV[[x]],4),
 								S.E.Est=round(S.E.Est[[x]],2),
 								S.E.Proc=round(S.E.Proc[[x]],2))
-						rownames(output)=c(as.character((nAdd+2):m),"Total")
+						rownames(output) <- c(as.character((nAdd+2):m),"Total")
 						return(output)
 						})
-		nm=paste("Summary Statistics for Triangle",c(as.character(1:p), paste(agg,sep="",collapse="+")),sep=" ")
-		names(output)=nm
+		nm <- paste("Summary Statistics for Triangle",c(as.character(1:p), paste(agg,sep="",collapse="+")),sep=" ")
+		names(output) <- nm
 		}
 	
 	return(output)    
@@ -254,9 +264,9 @@ summary.gmcl=function(object,agg=NULL,...){
 
 
 
-print.gmcl=function(x,...){
+print.GeneralMultiChainLadder <- function(x,...){
 	
-    summary.x <- summary.gmcl(x)
+    summary.x <- summary.GeneralMultiChainLadder(x)
     cat("\n")
     print(summary.x, quote=FALSE)
     invisible(x)
@@ -265,27 +275,27 @@ print.gmcl=function(x,...){
 
 
 # transform the Bcov from mcl to gmcl format
-transf.Bcov=function(Bcov.orig){
-	p=nrow(Bcov.orig)
-	Bcov.transf=matrix(0,p^2,p^2)
-	b=p*(1:p-1)+1:p
+transf.Bcov <- function(Bcov.orig){
+	p <- nrow(Bcov.orig)
+	Bcov.transf <- matrix(0,p^2,p^2)
+	b <- p*(1:p-1)+1:p
 	for (k in b) {
 		for (j in b){
-			pos1=which(b==k)
-			pos2=which(b==j)
-			Bcov.transf[k,j]=Bcov.orig[pos1,pos2]
+			pos1 <- which(b==k)
+			pos2 <- which(b==j)
+			Bcov.transf[k,j] <- Bcov.orig[pos1,pos2]
 		}
 	}
 	return(Bcov.transf)
 }
 
 #extract certain columns and put them to triangle format
-extract.cols=function(da.list, cols){
-	p=length(da.list)
-	da.list2=lapply(1:p,function(x) {
-		sel=da.list[[x]][,cols]
-		s=apply(is.na(sel),1,sum)
-		sel=sel[s<length(cols),]
+extract.cols <- function(da.list, cols){
+	p <- length(da.list)
+	da.list2 <- lapply(1:p,function(x) {
+		sel <- da.list[[x]][,cols]
+		s <- apply(is.na(sel),1,sum)
+		sel <- sel[s<length(cols),]
 		return(sel)
 		} )
 	return(da.list2)
--- R/multiChainLadder.R
+++ R/multiChainLadder.R
@@ -2,7 +2,7 @@
 ###################################################################################
 # Main function to fit model, predict, and calculate mse under MCL
 
-mcl <- function(triangles,
+MultiChainLadder <- function(triangles,
                 fit.method="SUR",
                 delta=1,
                 extrap="Mack",
@@ -18,86 +18,83 @@ mcl <- function(triangles,
     n <- dim(triangles[[1]])[2]
 
     ## fit the mcl model
-    models <- fit.mcl(triangles=triangles,
-                      fit.method =fit.method,
-                      delta=delta,
-                      extrap=extrap,...)
+    models <- fit.MultiChainLadder(triangles=triangles,
+                      		fit.method =fit.method,
+                      		delta=delta,
+                      		extrap=extrap,...)
 
     ## fill the triangle
-    fullTriangles <- predict.mclmodels(models)
+    fullTriangles <- predict(models)
 
     ## calculate the mse
-    mse <- mse.mcl(mse.method=mse.method,
-                   models=models,
-                   fullTriangles=fullTriangles)
+    mse <- mse.MultiChainLadder(mse.method=mse.method,
+                   		models=models,
+                   		fullTriangles=fullTriangles)
 
 
     output <- c(models,mse)
 
-    class(output) <- c("gmcl","list")
+    class(output) <- c("GeneralMultiChainLadder","list")
     return(output)
 }
 
 ######################################################################################
 # Fit regression models
 
-fit.mcl=function(triangles,
+fit.MultiChainLadder <- function(triangles,
 			fit.method="SUR",
 			delta=1,
 			extrap="Mack",...)
 	{
-	p=length(triangles)
-	m=dim(triangles[[1]])[1]
-	n=dim(triangles[[1]])[2]
+	p <- length(triangles)
+	m <- dim(triangles[[1]])[1]
+	n <- dim(triangles[[1]])[2]
 
-	B=Bcov=ecov=myModel=vector("list",n-1)
+	B <- Bcov <- ecov <- myModel <- vector("list",n-1)
 
 		for (i in 1:(n-1)){
-			y=x=system=vector("list",p)
+			y <- x <- system <- vector("list",p)
 			for (j in 1:p){
-				y[[j]]=triangles[[j]][1:(m-i),i+1]*(triangles[[j]][1:(m-i),i]^(-delta/2))
-				x[[j]]=triangles[[j]][1:(m-i),i]*(triangles[[j]][1:(m-i),i]^(-delta/2))
-				system[[j]]=as.formula(paste("y[[",j,"]]~-1+x[[",j,"]]",sep=""))
+				y[[j]] <- triangles[[j]][1:(m-i),i+1]*(triangles[[j]][1:(m-i),i]^(-delta/2))
+				x[[j]] <- triangles[[j]][1:(m-i),i]*(triangles[[j]][1:(m-i),i]^(-delta/2))
+				system[[j]] <- as.formula(paste("y[[",j,"]]~-1+x[[",j,"]]",sep=""))
 
 			}
 			if (!(i==n-1 & extrap!=FALSE )){
-				myModel[[i]]=systemfit(system,fit.method,...)
-				B[[i]]= as.vector(coef(myModel[[i]]))
-				Bcov[[i]]=unname(vcov(myModel[[i]]))
-				ecov[[i]]=unname(myModel[[i]]$residCov)
-				if (fit.method %in% "OLS") ecov[[i]]=diag(diag(ecov[[i]]),nrow=p)
+				myModel[[i]] <- systemfit(system,fit.method,...)
+				B[[i]] <-  as.vector(coef(myModel[[i]]))
+				Bcov[[i]] <- unname(vcov(myModel[[i]]))
+				ecov[[i]] <- unname(myModel[[i]]$residCov)
+				if (fit.method %in% "OLS") ecov[[i]] <- diag(diag(ecov[[i]]),nrow=p)
 				# replace off-diagonal elements as 0
 			}
 			else {
-				B[[i]]=sapply(1:p,function(x) triangles[[x]][1,i+1]/triangles[[x]][1,i])
+				B[[i]] <- sapply(1:p,function(x) triangles[[x]][1,i+1]/triangles[[x]][1,i])
 				if (extrap %in% "Genvar"){
-					ecov[[i]]=(det(ecov[[i-1]])^2/det(ecov[[i-2]]))^(1/p)*ecov[[i-1]]
+					ecov[[i]] <- (det(ecov[[i-1]])^2/det(ecov[[i-2]]))^(1/p)*ecov[[i-1]]
 				}
 				if (extrap %in% "Mack"){
-					r=abs(ecov[[i-1]]^2/ecov[[i-2]])
-					ecov[[i]]=pmin(abs(ecov[[i-2]]),abs(ecov[[i-1]]),replace(r,is.na(r),0))
+					r <- abs(ecov[[i-1]]^2/ecov[[i-2]])
+					ecov[[i]] <- pmin(abs(ecov[[i-2]]),abs(ecov[[i-1]]),replace(r,is.na(r),0))
 				}
-				Bcov[[i]]=solve(diag(unlist(x),nrow=p)%*%solve(ecov[[i]])%*%diag(unlist(x),nrow=p))
-				myModel[[i]]="Not applicable because of extrapolation"
+				Bcov[[i]] <- solve(diag(unlist(x),nrow=p)%*%solve(ecov[[i]])%*%diag(unlist(x),nrow=p))
+				myModel[[i]] <- "Not applicable because of extrapolation"
 			}
 		}
-	output=list()
-	output[["triangles"]]=triangles
-	output[["models"]]=myModel
-	output[["B"]]=B
-	output[["Bcov"]]=Bcov
-	output[["ecov"]]=ecov
-	output[["fit.method"]]=fit.method
-	output[["delta"]]=delta
-	output[["extrap"]]=extrap
-	class(output)="mclmodels"
+	output <- list()
+	output[["triangles"]] <- triangles
+	output[["models"]] <- myModel
+	output[["B"]] <- B
+	output[["Bcov"]] <- Bcov
+	output[["ecov"]] <- ecov
+	output[["fit.method"]] <- fit.method
+	output[["delta"]] <- delta
+	output[["extrap"]] <- extrap
+	class(output) <- "MultiChainLadderFit"
 	return(output)
 }
 
-########################################################################################
-# Complete the triangles
-
-predict.mclmodels=function(object,...)
+predict.MultiChainLadderFit=function(object,...)
 	{
 	triangles=object$triangles
 	B=object$B
@@ -106,7 +103,7 @@ predict.mclmodels=function(object,...)
 	n=dim(triangles[[1]])[2]
 	fullTriangles=triangles
     	for (i in 1:(n-1)){
-        	x=sapply(1:p, function(x) fullTriangles[[x]][(m-i+1):m,i])
+        	x=sapply(1:p, function(x) fullTriangles[[x]][(m-i+1):m,i])  
         	x=matrix(x,p,i,byrow=TRUE)
 			y=diag(B[[i]],nrow=p)%*%x
 			for (j in 1:p){
@@ -121,19 +118,19 @@ predict.mclmodels=function(object,...)
 # Compute the mse for each accident year
 
 
-mse.mcl=function(mse.method="Mack", models, fullTriangles)
+mse.MultiChainLadder <- function(mse.method="Mack", models, fullTriangles)
 	{
-	triangles=models$triangles
-	p=length(fullTriangles)
-	n=ncol(fullTriangles[[1]])
-	m=nrow(fullTriangles[[1]])
-	B=models$B
-	Bcov=models$Bcov
-	ecov=models$ecov
-	delta=models$delta
+	triangles <- models$triangles
+	p <- length(fullTriangles)
+	n <- ncol(fullTriangles[[1]])
+	m <- nrow(fullTriangles[[1]])
+	B <- models$B
+	Bcov <- models$Bcov
+	ecov <- models$ecov
+	delta <- models$delta
 
-	mse.ay=mse.ay.est=mse.ay.proc=matrix(0,m*p,n*p)
-	mse.total=mse.total.est=mse.total.proc=matrix(0,p,p*n)
+	mse.ay <- mse.ay.est <- mse.ay.proc <- matrix(0,m*p,n*p)
+	mse.total <- mse.total.est <- mse.total.proc <- matrix(0,p,p*n)
 
 
 	# mse for single accident years
@@ -141,30 +138,30 @@ mse.mcl=function(mse.method="Mack", models, fullTriangles)
 		{
 		for (i in m:(m+1-k))
 			{
-			a1=(p*(i-1)+1):(p*i)	 #old indexes
-			b1=(p*(k-1)+1):(p*k)
-			a2=(p*(i-1)+1):(p*i)	# new indexes
-			b2=(p*k+1):(p*(k+1))
+			a1 <- (p*(i-1)+1):(p*i)	 #old indexes
+			b1 <- (p*(k-1)+1):(p*k)
+			a2 <- (p*(i-1)+1):(p*i)	# new indexes
+			b2 <- (p*k+1):(p*(k+1))
 
-			yhat=sapply(1:p,function(x) fullTriangles[[x]][i,k])
+			yhat <- sapply(1:p,function(x) fullTriangles[[x]][i,k])
 			#process variance
-			mse.ay.proc[a2,b2]=diag(yhat^(delta/2),nrow=p)%*%ecov[[k]]%*%diag(yhat^(delta/2),nrow=p)+
+			mse.ay.proc[a2,b2] <- diag(yhat^(delta/2),nrow=p)%*%ecov[[k]]%*%diag(yhat^(delta/2),nrow=p)+
 								B[[k]]%*%t(B[[k]])*mse.ay.proc[a1,b1]
 
 			#estimation variance
 			if (mse.method %in% "Mack")	#Mack formulas
 				{
-				mse.ay.est[a2,b2]=Bcov[[k]]*(yhat%*%t(yhat))+
+				mse.ay.est[a2,b2] <- Bcov[[k]]*(yhat%*%t(yhat))+
 									(B[[k]]%*%t(B[[k]]))*mse.ay.est[a1,b1]
 				}
 			if (mse.method %in% "Murphy") #generalized Murphy formulas
 				{
-				mse.ay.est[a2,b2]=Bcov[[k]]*(yhat%*%t(yhat))+
+				mse.ay.est[a2,b2] <- Bcov[[k]]*(yhat%*%t(yhat))+
 									(B[[k]]%*%t(B[[k]]))*mse.ay.est[a1,b1]+
 									Bcov[[k]]*mse.ay.est[a1,b1]
 				}
 			#mse
-			mse.ay[a2,b2]=mse.ay.proc[a2,b2]+mse.ay.est[a2,b2]
+			mse.ay[a2,b2] <- mse.ay.proc[a2,b2]+mse.ay.est[a2,b2]
 			}
 		}
 
@@ -172,35 +169,35 @@ mse.mcl=function(mse.method="Mack", models, fullTriangles)
 	for (k in 1:(n-1))
 		{
 
-		b1=(p*(k-1)+1):(p*k)
-		b2=(p*k+1):(p*(k+1))
-		proc=matrix(0,p,p)
+		b1 <- (p*(k-1)+1):(p*k)
+		b2 <- (p*k+1):(p*(k+1))
+		proc <- matrix(0,p,p)
 
 		#process variance
 		for (i in ((m+1-k):m))
 			{
-			yhat=sapply(1:p,function(x) fullTriangles[[x]][i,k])
-			proc=proc+diag(yhat^(delta/2),nrow=p)%*%ecov[[k]]%*%diag(yhat^(delta/2),nrow=p)
+			yhat <- sapply(1:p,function(x) fullTriangles[[x]][i,k])
+			proc <- proc+diag(yhat^(delta/2),nrow=p)%*%ecov[[k]]%*%diag(yhat^(delta/2),nrow=p)
 			}
 
-		sumyhat=sapply(1:p,function(x) sum(fullTriangles[[x]][(m+1-k):m,k]))
-		mse.total.proc[,b2]=proc+B[[k]]%*%t(B[[k]])*mse.total.proc[,b1]
+		sumyhat <- sapply(1:p,function(x) sum(fullTriangles[[x]][(m+1-k):m,k]))
+		mse.total.proc[,b2] <- proc+B[[k]]%*%t(B[[k]])*mse.total.proc[,b1]
 
 		#estimation variance
 		if (mse.method == "Mack")
 			{
-			mse.total.est[,b2]=Bcov[[k]]*(sumyhat%*%t(sumyhat))+
+			mse.total.est[,b2] <- Bcov[[k]]*(sumyhat%*%t(sumyhat))+
 									(B[[k]]%*%t(B[[k]]))*mse.total.est[,b1]
 			}
 		if (mse.method == "Murphy")
 			{
-			mse.total.est[,b2]=Bcov[[k]]*(sumyhat%*%t(sumyhat))+
+			mse.total.est[,b2] <- Bcov[[k]]*(sumyhat%*%t(sumyhat))+
 									(B[[k]]%*%t(B[[k]]))*mse.total.est[,b1]+
 									Bcov[[k]]*mse.total.est[,b1]
 			}
 
 		#total variance
-		mse.total[,b2]=mse.total.proc[,b2]+mse.total.est[,b2]
+		mse.total[,b2] <- mse.total.proc[,b2]+mse.total.est[,b2]
 		}
 
 
