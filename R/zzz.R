1d32a429f21f57b8afab8a70f0945f91e2759a11
markus.gesmann <markus.gesmann@e5f87290-e53f-0410-917e-5545ed4be688>
2010-03-14 23:15:29 +0000

    Added demo files for ChainLadder package, MackChainLadder, DatabaseExamples, MSOffice. I believe demo files are a better solution than bloated examples.
    git-svn-id: https://chainladder.googlecode.com/svn/trunk@232 e5f87290-e53f-0410-917e-5545ed4be688

--- R/zzz.R
+++ R/zzz.R
@@ -2,11 +2,12 @@
 {
     require(utils)
     cat("\n",paste("ChainLadder version", packageDescription("ChainLadder")$Version,
-                   "by Markus Gesmann <markus.gesmann@gmail.com>,\nWayne (Yanwei) Zhang <actuaryzhang@uchicago.edu>\n\n"),
+                   "by:\nMarkus Gesmann <markus.gesmann@gmail.com>,\nWayne (Yanwei) Zhang <actuaryzhang@uchicago.edu>\n\n"),
         "Type library(help='ChainLadder') or ?ChainLadder\n",
         "to see overall documentation.\n\n",
-        "Type example(ChainLadder) to get an idea of the functionality of this package.\n\n",
-        "Feel free to send me an email if you would like to keep ",
+        "Type demo(ChainLadder) to get an idea of the functionality of this package.\n\n",
+        "See demo(package='ChainLadder') for a list of more demos.\n\n",
+        "Feel free to send us an email if you would like to keep ",
         "informed of\nnew versions or if you have any feedback, ",
         "ideas, suggestions or ",
         "would\nlike to collaborate.\n\n",
---
+++ demo/MSOffice.R
@@ -0,0 +1,19 @@
+## Demo which shows how to use R and ChainLadder for auto reporting
+## Markus Gesmann, March 2010
+## See also the R2PPT package
+## Of course you need MS Office, and also the 'rcom' package
+R <- MackChainLadder(RAA)
+myfile=tempfile()
+win.metafile(file=myfile)
+plot(R)
+dev.off()
+#
+require(rcom)
+ppt<-comCreateObject("Powerpoint.Application")
+comSetProperty(ppt,"Visible",TRUE)
+myPresColl<-comGetProperty(ppt,"Presentations")
+myPres<-comInvoke(myPresColl,"Add")
+mySlides<-comGetProperty(myPres,"Slides")
+mySlide<-comInvoke(mySlides,"Add",1,12)
+myShapes<-comGetProperty(mySlide,"Shapes")
+myPicture<-comInvoke(myShapes,"AddPicture",myfile,0,1,100,10)
