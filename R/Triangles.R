4788bedc29116c8352822f6c59bf3755bc13589d
chiefmurphy@gmail.com <chiefmurphy@gmail.com@e5f87290-e53f-0410-917e-5545ed4be688>
2012-11-06 06:08:18 +0000

    as.triangle.data.frame uses the acast function from the reshape2 package, which allows for the aggregation of observed losses from the same combination of origin and dev -- as with, for example, detailed claims or claims by state.
    git-svn-id: https://chainladder.googlecode.com/svn/trunk@384 e5f87290-e53f-0410-917e-5545ed4be688

--- R/Triangles.R
+++ R/Triangles.R
@@ -58,7 +58,10 @@ as.triangle.data.frame <- function(Triangle, origin="origin", dev="dev", value="
         matrixTriangle <- as.matrix(Triangle)
         matrixTriangle <- as.triangle(matrixTriangle)
     }else{
-        matrixTriangle <- .as.MatrixTriangle(Triangle, origin, dev, value)
+        fmla <- as.formula(paste(origin, "~", dev))
+        matrixTriangle <- acast(Triangle, fmla, fun.aggregate = sum, value.var = value, fill = as.numeric(NA))
+        names(dimnames(matrixTriangle)) <- c(origin, dev)
+#        matrixTriangle <- .as.MatrixTriangle(Triangle, origin, dev, value)
     }
     class(matrixTriangle) <- c("triangle", "matrix")
     return(matrixTriangle)
