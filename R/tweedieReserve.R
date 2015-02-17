0481531193867597fcd05b2babfb662df47dfe01
Markus Gesmann <markus.gesmann@gmail.com>
2015-02-17 21:53:42 +0000

    Made code more readable: added spaces around parameters and replaced '=' where there were asignement with '<-'.
    Added additional if statement in print.tweedieReserve to cater for the case rereserving = FALSE and bootstrap=0

--- R/tweedieReserve.R
+++ R/tweedieReserve.R
@@ -89,7 +89,7 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
   tr.incr <- if (cum) cum2incr(triangle) else triangle
   
   # create family                            
-  family <- tweedie(var.power=var.power,link.power=link.power)
+  family <- statmod::tweedie(var.power=var.power, link.power=link.power)
   
   # convert to long format
   lda <-  as.data.frame(tr.incr)
@@ -98,11 +98,11 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
       family$linkfun(attr(tr.incr,"exposure")[lda$origin])
   
   #parameter fix for better intrepretation of results
-  base.year=min(lda$origin)
-  lda$origin<-lda$origin-base.year+1 # ORIGIN MUST START FROM 1
+  base.year <- min(lda$origin)
+  lda$origin <- lda$origin - base.year + 1 # ORIGIN MUST START FROM 1
   
-  base.dev=min(lda$dev)
-  lda$dev<-lda$dev-base.dev  # DEV MUST START FROM 1
+  base.dev <- min(lda$dev)
+  lda$dev <- lda$dev - base.dev  # DEV MUST START FROM 1
   lda$cy <- lda$origin + lda$dev
   
   ######################################
@@ -111,126 +111,134 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
   
   
   #  design matrix per AY
-  design.string<-" "
-  m_dev=max(lda["dev"]) ##max_dev (IT MUST START from 0!!!)
-  m_cy=2*m_dev
-  m=nrow(lda)
+  design.string <- " "
+  m_dev <- max(lda["dev"]) ##max_dev (IT MUST START from 0!!!)
+  m_cy <- 2*m_dev
+  m <- nrow(lda)
   
-  temp<-rep(1,m)
+  temp <- rep(1,m)
   
   if (design.type[1] == 0){
     ay<-matrix(0,nrow=m,ncol=1)
   }
   if (design.type[1] == 1){
     
-    design.string=paste(design.string,"factor(origin)")
+    design.string <- paste(design.string,"factor(origin)")
     
-    ay<-matrix(0,nrow=m, ncol=m_dev)
+    ay <- matrix(0, nrow=m, ncol=m_dev)
     for(r in 1:m){
-      if (lda$origin[r]!=1) {ay[r,lda$origin[r]-1]=1}
+      if (lda$origin[r]!=1) {
+        ay[r,lda$origin[r]-1] <- 1
+      }
     }
-    ay<-cbind(temp,ay)       
+    ay <- cbind(temp,ay)       
   }
   if (design.type[1] == 2){
     
-    design.string=paste(design.string,"origin")
+    design.string <- paste(design.string, "origin")
     
-    ay<-matrix(0,nrow=m, ncol=1)
+    ay <- matrix(0,nrow=m, ncol=1)
     for(r in 1:m){
-      ay[r,1]=lda$origin[r]
+      ay[r, 1] <- lda$origin[r]
     }
-    ay<-cbind(temp,ay)
+    ay<-cbind(temp, ay)
   }
   
   #  design matrix per DY
   if (design.type[2] == 0){
-    dy<-matrix(0,nrow=m,ncol=1)
+    dy <- matrix(0, nrow=m, ncol=1)
   }
   if (design.type[2] == 1){
     
     if (design.type[1]==0) {
-      design.string=paste(design.string,"factor(dev)")
+      design.string <- paste(design.string, "factor(dev)")
     } else {
-      design.string=paste(design.string,"+ factor(dev)")
+      design.string <- paste(design.string, "+ factor(dev)")
     }
     
-    dy<-matrix(0,nrow=m, ncol=m_dev)
+    dy <- matrix(0,nrow=m, ncol=m_dev)
     for(r in 1:m){
-      if (lda$dev[r]!=0) { dy[r,lda$dev[r]]=1 }
+      if (lda$dev[r]!=0) { 
+        dy[r,lda$dev[r]] <- 1 
+      }
     }
-    dy<-cbind(temp,dy)
+    dy <- cbind(temp, dy)
     
   }
   if (design.type[2] == 2){
     
     if (design.type[1]==0) {
-      design.string=paste(design.string,"dev") 
+      design.string <- paste(design.string, "dev") 
     } else {
-      design.string=paste(design.string,"+ dev")
+      design.string <- paste(design.string,"+ dev")
     }
     
     dy<-matrix(0,nrow=m, ncol=1)
     for(r in 1:m){
-      dy[r,1]=lda$dev[r]
+      dy[r, 1] <- lda$dev[r]
     }
     dy<-cbind(temp,dy)
   }
   
   #  design matrix per CY
   if (design.type[3] == 0){
-    cy<-matrix(0,nrow=m,ncol=1)
+    cy <- matrix(0, nrow=m, ncol=1)
   }
   if (design.type[3] == 1){
     
     if (design.type[1]==0 && design.type[2]==0) {
-      design.string=paste(design.string,"factor(cy)")
+      design.string <- paste(design.string, "factor(cy)")
     } else {
-      design.string=paste(design.string,"+ factor(cy)")
+      design.string <- paste(design.string, "+ factor(cy)")
     }
     
-    cy<-matrix(0,nrow=m, ncol=2*m_dev)
+    cy <- matrix(0,nrow=m, ncol=2*m_dev)
     for(r in 1:m) {
-      if (lda$cy[r]!=1) {cy[r,lda$cy[r]-1]=1}
+      if (lda$cy[r]!=1) {
+        cy[r,lda$cy[r]-1] <- 1
+      }
     }
-    cy<-cbind(temp,cy)
+    cy <- cbind(temp,cy)
     
   }
   if (design.type[3] == 2){
     
     if (design.type[1]==0 && design.type[2]==0) {
-      design.string=paste(design.string,"cy")
+      design.string <- paste(design.string, "cy")
     } else {
-      design.string=paste(design.string,"+ cy")
+      design.string <- paste(design.string, "+ cy")
     }
     
-    cy<-matrix(0,nrow=m, ncol=1)
+    cy <- matrix(0, nrow=m, ncol=1)
     for(r in 1:m){
-      cy[r,1]=lda$cy[r]
+      cy[r, 1] <- lda$cy[r]
     }
-    cy<-cbind(temp,cy)
+    cy <- cbind(temp, cy)
     
   }
   
-  design.matrix<-cbind(ay,dy,cy)
-  design.matrix<-cbind(temp,subset(design.matrix, 
-                                   select=-c(1,length(ay[1,])+1,
-                                             (length(ay[1,])+length(dy[1,])+1))))
+  design.matrix <- cbind(ay, dy, cy)
+  design.matrix <- cbind(temp, subset(design.matrix, 
+                                   select=-c(1, length(ay[1,]) + 1,
+                                             (length(ay[1,]) + length(dy[1,])+1))
+                                   )
+                         )
   
   ######################################
   ####END DESIGN MATRIX CALCULATION#####
   ######################################
   
   
-  ldaFit <- subset(lda,!is.na(lda$value)) 
-  ldaOut <- subset(lda,is.na(lda$value))
+  ldaFit <- subset(lda, !is.na(lda$value)) 
+  ldaOut <- subset(lda, is.na(lda$value))
   
   # fit the model
   #glmFit<-glm(value~factor(origin)+factor(dev),family=quasipoisson(log),data=ldaFit)
   
   if (exposure){
-    glmstring<-paste(design.string,", family = family, data=ldaFit,offset=offset")
+    glmstring <- paste(design.string,", family = family, data=ldaFit,offset=offset")
   } else {
-    glmstring<-paste(design.string,", family = family, data=ldaFit")
+    glmstring <- paste(design.string,", family = family, data=ldaFit")
   }
   
   eval(parse(text=
@@ -248,15 +256,15 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
   # prediction for each cell
   
   coeffs <- glmFit$coefficients
-  temp_y<-data.frame(gamma_y<-c(0))
+  temp_y <- data.frame(gamma_y<-c(0))
   
   # an extrapultion of future CY factors has to be made for the future CY
   if (design.type[3]==1) {
-    temp_y<-fit_gamma(coeffs,design.type,n=m_dev) ## NOTE: 0 included!!! so actually we have (n+1) values!!
-    coeffs<-temp_y$coeffs
+    temp_y <- fit_gamma(coeffs, design.type, n=m_dev) ## NOTE: 0 included!!! so actually we have (n+1) values!!
+    coeffs <- temp_y$coeffs
   }
   n <- nrow(ldaFit) ## nÂ° of data points (n)
-  d.f<- df.residual(glmFit) ## nÂ° of data points - parameters (n-p)
+  d.f <- df.residual(glmFit) ## nÂ° of data points - parameters (n-p)
   bias <- sqrt(n/d.f)
   
   # dispersion
@@ -264,17 +272,19 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
   phi <- summary(glmFit)$dispersion
   
   lda$eta <- design.matrix %*% coeffs
-  if(exposure) {lda$eta <- lda$eta + lda$offset}
+  if(exposure) {
+    lda$eta <- lda$eta + lda$offset
+  }
   lda$yp <- exp(lda$eta)  
   
   # sum to get reserve by year
   
-  resMeanAy <- tapply(lda$yp[is.na(lda$value)],ldaOut$origin, sum)
+  resMeanAy <- tapply(lda$yp[is.na(lda$value)], ldaOut$origin, sum)
   resMeanTot <- sum(resMeanAy)       
   
-  Reserve <- round(c(resMeanAy,resMeanTot))
+  Reserve <- round(c(resMeanAy, resMeanTot))
   Latest <- getLatestCumulative(incr2cum(tr.incr))[-1L]
-  Latest <- c(Latest,sum(Latest))
+  Latest <- c(Latest, sum(Latest))
   Ultimate <- Latest + Reserve
   
   
@@ -286,12 +296,12 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
     if(progressBar){
       pb <- txtProgressBar(min = 1, max = nsim, style=3)
     }
-    resMeanAyB <- matrix(0,length(resMeanAy),nsim)
-    resMeanTotB <- rep(0,nsim)
+    resMeanAyB <- matrix(0, length(resMeanAy), nsim)
+    resMeanTotB <- rep(0, nsim)
     
     if (rereserving) { ##CREATE 1yr matrix
-      resMeanAyB_1yr <- matrix(0,length(resMeanAy),nsim)
-      resMeanTotB_1yr <- rep(0,nsim)
+      resMeanAyB_1yr <- matrix(0, length(resMeanAy), nsim)
+      resMeanTotB_1yr <- rep(0, nsim)
       
     }
     
@@ -302,9 +312,9 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
       ### PARAMETRIC BOOTSTRAP ###
       if(bootstrap==1) {
         if(var.power!=0){ ### rtweedie doesn't support var.power = 0
-          yB=rtweedie(n,phi=phi,xi=var.power,mu=mu) 
+          yB=rtweedie(n, phi=phi, xi=var.power, mu=mu) 
         } else{
-          yB=rnorm(n,mean=mu,sd=sqrt(phi)) #### KNOWN ISSUE: Error in glm.fit with negative values .. why?
+          yB=rnorm(n, mean=mu, sd=sqrt(phi)) #### KNOWN ISSUE: Error in glm.fit with negative values .. why?
         }
       } else { ### SEMI-PARAMETRIC BOOTSTRAP ###      
         if(boot.adj==0){  ### WHILE CYCLE until triangle > 0 ... WARNING: COULD BE TIME CONSUMING!!!!
@@ -312,7 +322,9 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
           tries=0
           while (ybad){
             tries = tries + 1
-            if (tries>100) {stop("Too many negative bootstrapped values!!")} ### Added to exit code, otherwise "infinite" WHILE cycle could happen!!
+            if (tries>100) {
+              stop("Too many negative bootstrapped values!!")
+              } ### Added to exit code, otherwise "infinite" WHILE cycle could happen!!
             
             rn <- bias * sample(resid(glmFit, type = "pearson"), n, replace = TRUE)
             yB <- rn * sqrt(family$variance(mu)) + mu
@@ -326,9 +338,9 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
           yB <- rn * sqrt(family$variance(mu)) + mu      
           if (!all(yB>=0) || (!is.null(var.power) && var.power != 0)) {
             for (i in 1:n){
-              if (yB[i]<=0) {        
-                yB[i]<-0.01
-                count.neg<-count.neg+1
+              if (yB[i] <= 0) {        
+                yB[i] <- 0.01
+                count.neg <- count.neg + 1
               }
             }
           }
@@ -347,41 +359,44 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
       coeffsB <- glmFitB$coefficients
       
       if (design.type[3]==1) {
-        temp_yB<-fit_gamma(coeffsB,design.type,n=m_dev) ## NOTE: DEV must start with 0!!!
-        coeffsB<-temp_yB$coeffs
+        temp_yB <- fit_gamma(coeffsB, design.type, n=m_dev) ## NOTE: DEV must start with 0!!!
+        coeffsB <- temp_yB$coeffs
       }
       
       lda$etaB <- design.matrix %*% coeffsB
-      if(exposure) {lda$etaB <- lda$etaB + lda$offset}
+      if(exposure) {
+        lda$etaB <- lda$etaB + lda$offset
+      }
       lda$ypB <- exp(lda$etaB)
       
       ##ADD PROCESS ERROR
       if (proc.err){
-        n.fut = length(lda$ypB[is.na(lda$value)])
-        mu.fut= lda$ypB[is.na(lda$value)]
+        n.fut <- length(lda$ypB[is.na(lda$value)])
+        mu.fut <- lda$ypB[is.na(lda$value)]
         if (var.power!=0){
-          lda$ypB[is.na(lda$value)]=rtweedie(n.fut,xi=var.power,mu=mu.fut,phi=phi)  
+          lda$ypB[is.na(lda$value)] <- rtweedie(n.fut, xi=var.power,
+                                                mu=mu.fut, phi=phi)  
         } else {
-          lda$ypB[is.na(lda$value)]=rnorm(n.fut,mean=mu.fut,sd=sqrt(phi))
+          lda$ypB[is.na(lda$value)] <- rnorm(n.fut, mean=mu.fut, sd=sqrt(phi))
         }
       }
       
       
-      resMeanAyB[,b] <- tapply(lda$ypB[is.na(lda$value)],ldaOut$origin, sum)
+      resMeanAyB[,b] <- tapply(lda$ypB[is.na(lda$value)], ldaOut$origin, sum)
       resMeanTotB[b] <- sum(resMeanAyB[,b])
       
       ### 1yr VIEW Calculation ###
       if (rereserving) {
         
-        lda$year1=lda$value  ## COPY ALL THE KNOWN VALUES
+        lda$year1 <- lda$value  ## COPY ALL THE KNOWN VALUES
         next_y <- max(lda$origin)+1 ##NEXT DIAGONAL is for CY = max(origin)+1
         lda$year1[lda$cy==next_y]=lda$ypB[lda$cy==next_y]  ##COPY NEXT DIAGONAL
-        ldaFit_1yr = subset(lda,!is.na(lda$year1))
+        ldaFit_1yr <- subset(lda,!is.na(lda$year1))
         
         if (exposure){
-          glmstring_1yr<-paste(design.string,", family = family, data=ldaFit_1yr,offset=offset")
+          glmstring_1yr <- paste(design.string,", family = family, data=ldaFit_1yr,offset=offset")
         } else{
-          glmstring_1yr<-paste(design.string,", family = family, data=ldaFit_1yr")
+          glmstring_1yr <- paste(design.string,", family = family, data=ldaFit_1yr")
         }
         
         eval(parse(text=
@@ -395,17 +410,20 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
         coeffs1yr <- glmFit1yr$coefficients
         
         if (design.type[3]==1) {
-          temp_1yr<-fit_gamma(coeffs1yr,design.type,n=m_dev) ## NOTE: 0 included!!! so actually we have (n+1) values!!
-          coeffs1yr<-temp_1yr$coeffs
+          temp_1yr <- fit_gamma(coeffs1yr,design.type,n=m_dev) ## NOTE: 0 included!!! so actually we have (n+1) values!!
+          coeffs1yr <- temp_1yr$coeffs
         }
         
         lda$be1yr <- design.matrix %*% coeffs1yr ##ETA
-        if(exposure) {lda$be1yr <- lda$be1yr + lda$offset}        ##ETA + OFFSET
+        if(exposure) {
+          lda$be1yr <- lda$be1yr + lda$offset
+          }        ##ETA + OFFSET
         lda$be1yr <- exp(lda$be1yr)              ##Best Estimate 1yr, for all the values ..
         
-        lda$year1[is.na(lda$year1)]=lda$be1yr[is.na(lda$year1)]
+        lda$year1[is.na(lda$year1)] <- lda$be1yr[is.na(lda$year1)]
         
-        resMeanAyB_1yr[,b] <- tapply(lda$year1[is.na(lda$value)],ldaOut$origin, sum)
+        resMeanAyB_1yr[,b] <- tapply(lda$year1[is.na(lda$value)],
+                                     ldaOut$origin, sum)
         resMeanTotB_1yr[b] <- sum(resMeanAyB_1yr[,b])        
       }
       ##setWinProgressBar(pb, b, title=paste(round(b/nsim*100, 0),"% Done"))
@@ -415,27 +433,27 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
     } ## end nsim look
     
     ##the adjustment for DF is included directly in residuals (see England(2002) Addendum) or in parametric bootstrap
-    mseEstAy <- apply(resMeanAyB,1,var)
+    mseEstAy <- apply(resMeanAyB, 1, var)
     mseEstTot <- var(resMeanTotB)
     
-    avgResAy <- apply(resMeanAyB,1,mean)
+    avgResAy <- apply(resMeanAyB, 1, mean)
     avgResTot <- mean(resMeanTotB)
     
-    Expected.Reserve <- round(c(avgResAy,avgResTot))
+    Expected.Reserve <- round(c(avgResAy, avgResTot))
     
-    S.E <- sqrt(c(mseEstAy,mseEstTot))
+    S.E <- sqrt(c(mseEstAy, mseEstTot))
     CoV <- S.E / Expected.Reserve
     
     if (rereserving) {
-      mseEstAy_1yr <- apply(resMeanAyB_1yr,1,var)
+      mseEstAy_1yr <- apply(resMeanAyB_1yr, 1, var)
       mseEstTot_1yr <- var(resMeanTotB_1yr)
       
-      avgResAy_1yr <- apply(resMeanAyB_1yr,1,mean)
+      avgResAy_1yr <- apply(resMeanAyB_1yr, 1, mean)
       avgResTot_1yr <- mean(resMeanTotB_1yr)
       
-      Expected.Reserve_1yr <- round(c(avgResAy_1yr,avgResTot_1yr))
+      Expected.Reserve_1yr <- round(c(avgResAy_1yr, avgResTot_1yr))
       
-      S.E_1yr <- sqrt(c(mseEstAy_1yr,mseEstTot_1yr))
+      S.E_1yr <- sqrt(c(mseEstAy_1yr, mseEstTot_1yr))
       CoV_1yr <- S.E_1yr / Expected.Reserve_1yr
       
       Emergence.Pattern <- S.E_1yr/S.E
@@ -451,28 +469,28 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
   if (bootstrap!=0) {
     if (rereserving) {
       resDf <- data.frame(Latest=Latest, 
-                          Expected.Reserve=Expected.Reserve,
-                          Prediction.Error=S.E,
+                          IBNR=Expected.Reserve,
+                          IBNR.S.E=S.E,
                           CoV=CoV,
-                          Expected.Ultimate=Latest+Expected.Reserve,
-                          Det.Reserve=Reserve,
-                          Expected.Reserve_1yr=Expected.Reserve_1yr,
-                          Prediction.Error_1yr=S.E_1yr,
+                          Ultimate=Latest+Expected.Reserve,
+                          Det.IBNR=Reserve,
+                          CDR=Expected.Reserve_1yr,
+                          CDR.S.E=S.E_1yr,
                           Emergence.Pattern = Emergence.Pattern,
                           Dev.To.Date=Latest/Ultimate)                     
     } else {
       resDf <- data.frame(Latest=Latest, 
-                          Expected.Reserve=Expected.Reserve,
-                          Prediction.Error=S.E,
+                          IBNR=Expected.Reserve,
+                          IBNR.S.E=S.E,
                           CoV=CoV,
-                          Expected.Ultimate=Latest+Expected.Reserve,
-                          Det.Reserve=Reserve,
+                          Ultimate=Latest+Expected.Reserve,
+                          Det.IBNR=Reserve,
                           Dev.To.Date=Latest/Ultimate)
     }
     
   } else {
     resDf <- data.frame(Latest=Latest, 
-                        Det.Reserve=Reserve,
+                        Det.IBNR=Reserve,
                         Ultimate=Ultimate,
                         Dev.To.Date=Latest/Ultimate)
   }
@@ -503,9 +521,10 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
             GLMReserve=resMeanTot,
             gamma_y=temp_y$gamma_y,
             res.diag=res.diag,
-            rereserving=rereserving)
+            rereserving=rereserving,
+            bootstrap=bootstrap)
   
-  if (bootstrap!=0) {
+  if (bootstrap != 0) {
     if (rereserving) {
       out<-c(out,list(distr.res_ult=resMeanTotB,
                       distr.res_1yr=resMeanTotB_1yr
@@ -519,12 +538,16 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
   
   if (p.optim){
     #library(tweedie)
-    p<-tweedie.profile(value~as.factor(dev)+cy,p.vec=c(seq(0, 2, by=0.1),3),data=ldaFit,offset=offset,do.plot=T)
+    p <- tweedie.profile(value ~ as.factor(dev)+cy,
+                         p.vec = c(seq(0, 2, by=0.1),3),
+                         data = ldaFit, 
+                         offset = offset,
+                         do.plot = TRUE)
     
     if (exposure){
-      glmstring<-paste(design.string,", family = family, data=ldaFit,p.vec=c(seq(0, 2, by=0.1),3),do.plot=T,offset=offset")
+      glmstring<-paste(design.string,", family = family, data=ldaFit,p.vec=c(seq(0, 2, by=0.1),3), do.plot=TRUE, offset=offset")
     } else {
-      glmstring<-paste(design.string,", family = family, data=ldaFit,p.vec=c(seq(0, 2, by=0.1),3),do.plot=T")
+      glmstring<-paste(design.string,", family = family, data=ldaFit,p.vec=c(seq(0, 2, by=0.1),3), do.plot=TRUE")
     }
     eval(parse(text=
                  c(
@@ -542,17 +565,18 @@ tweedieReserve <- function(triangle, var.power=1, link.power=0,
 
 print.tweedieReserve <- function(x,...){
   print(x$call)
-  if(x$rereserving){      
-    out <- x$summary[c("Latest", "Expected.Reserve", 
-                       "Prediction.Error", "Prediction.Error_1yr")]
-    names(out) <- c("Latest", "IBNR", "IBNR.S.E", "CDR.S.E")
-  }else{    
-    out <- x$summary[c("Latest", "Expected.Reserve", 
-                       "Prediction.Error")]
-    names(out) <- c("Latest", "IBNR", "IBNR.S.E")
+  if (x$bootstrap != 0) {
+    if (x$rereserving) {     
+      out <- x$summary[c("Latest", "IBNR", "IBNR.S.E", "CDR.S.E")]
+    }
+    else{    
+      out <- x$summary[c("Latest", "IBNR", "IBNR.S.E")]
+    }
   }
-  print(out)
-  
+  else{
+    out <- x$summary[c("Latest", "Det.IBNR", "Ultimate")]
+  }
+  print(out)  
 }
 
 summary.tweedieReserve <- function(object,
