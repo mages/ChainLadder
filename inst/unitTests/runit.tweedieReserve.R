b5e552542a270b9a4a2c9fbad43e40360cd57871
Markus Gesmann <markus.gesmann@gmail.com>
2015-03-02 07:53:02 +0000

    Updated tests for tweedieReserve

--- inst/unitTests/runit.tweedieReserve.R
+++ inst/unitTests/runit.tweedieReserve.R
@@ -2,28 +2,26 @@ test.tweedieReserve <- function() {
   set.seed(123)
   #ODP, PARAMETRIC BOOTSTRAP
   TR_1 <- tweedieReserve(MW2008,nsims=10000,rereserving=TRUE)
-  rep_1<-RC_report(TR_1)
+  rep_1<- summary(TR_1)
   
-  ult_1 <- c(2238807.8134,127632.394,0.0585,2206318.0016,2238345.1984,2309516.7468,2402395.6175,2458976.9985,2590679.9489,2611995.8277)
-  one_1 <- c(2238824.0789,108079.4533,0.0479,2205981.4698,2234598.5035,2297152.4462,2375946.8734,2416920.3086,2509737.9688,2563451.1534)
-  diag_1 <- c(2237826,2238808,2238824)
-  
-  checkEquals(rep_1$Ultimate_View, ult_1,tol=0.01, checkNames = FALSE)
-  checkEquals(rep_1$'1yr_View', one_1,tol=0.01, checkNames = FALSE)
+  ult_1 <- c(2239822.0, 127632.4, 2245462.4, 2327309.6, 2398481.2, 2448301.3, 2580004.2)
+  one_1 <- c(2238888.5, 108079.5, 2238215.1, 2310797.3, 2367667.1, 2416964.8, 2520365.7)
+  diag_1 <- c(2237826, 2239822, 2238889)
+  checkEquals(rep_1$Prediction[['IBNR']], ult_1,tol=0.01, checkNames = FALSE)
+  checkEquals(rep_1$Prediction[['CDR(1)']], one_1,tol=0.01, checkNames = FALSE)
   checkEquals(rep_1$Diagnostic,diag_1,tol=0.01, checkNames = FALSE)
   
   #Arithmetic separation method, PARAMETRIC BOOTSTRAP
   set.seed(123)
   TR_2 <- tweedieReserve(MW2008,nsims=10000,rereserving=TRUE,design.type=c(0,1,1))
-  rep_2<-RC_report(TR_2)
+  rep_2<- summary(TR_2)
   
-  ult_2 <- c(2210254.6134,120120.2424,0.0543,2180409.7893,2206455.6401,2269709.8490,2366451.5802,2415008.4877,2504159.7139,2614654.7706)
-  one_2 <- c(2209135.3774,109864.4061,0.0497,2183977.6237,2209053.0242,2262391.5951,2350931.6847,2384833.0428,2500526.3293,2587117.8086)
-  diag_2 <- c(2213850,2210255,2209135)
+  ult_2 <- c(2210254.6, 120120.2, 2206455.6, 2288314.0, 2366451.6, 2415008.5, 2504159.7)
+  one_2 <- c(2209135.4, 109864.4, 2209053.0, 2280793.9, 2350931.7, 2384833.0, 2500526.3)
+  diag_2 <- c(2213850, 2210255, 2209135 )
   
-  checkEquals(rep_2$Ultimate_View, ult_2,tol=0.01, checkNames = FALSE)
-  checkEquals(rep_2$'1yr_View', one_2,tol=0.01, checkNames = FALSE)
+  checkEquals(rep_2$Prediction[['IBNR']], ult_2,tol=0.01, checkNames = FALSE)
+  checkEquals(rep_2$Prediction[['CDR(1)']], one_2,tol=0.01, checkNames = FALSE)
   checkEquals(rep_2$Diagnostic,diag_2,tol=0.01, checkNames = FALSE)
-  
   
 }
