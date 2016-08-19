4d45cefc7c5d1c4ef4b123bc3e13e25e2d238070
Dan Murphy <danielmarkmurphy@gmail.com>
2016-08-19 10:03:36 -0700

    Test unnamed and named exposures

--- inst/unitTests/runit.glmReserving.r
+++ inst/unitTests/runit.glmReserving.r
@@ -1,8 +1,11 @@
 test.glmReserve.DateLabels <- function() {
-  expos <- (7 + 1:10 * 0.4) * 100
+  expos <- (7 + 1:10 * 0.4) * 10
   GenIns2 <- GenIns
   rownames(GenIns2) <- paste0(2001:2010, "-01-01")
   attr(GenIns2, "exposure") <- expos
-  (fit4 <- glmReserve(GenIns2))
+  checkException(fit5 <- glmReserve(GenIns2))
+  names(expos) <- rownames(GenIns2)
+  attr(GenIns2, "exposure") <- expos
+  (fit6 <- glmReserve(GenIns2))
   }
 
