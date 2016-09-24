a9d56e1cd4e3d36f568fca42470e71cb4f313a24
Markus Gesmann <markus.gesmann@gmail.com>
2016-09-24 12:10:30 +0100

    Excluded checkException statemtent
    Is this required?

--- inst/unitTests/runit.glmReserving.r
+++ inst/unitTests/runit.glmReserving.r
@@ -3,7 +3,7 @@ test.glmReserve.DateLabels <- function() {
   GenIns2 <- GenIns
   rownames(GenIns2) <- paste0(2001:2010, "-01-01")
   attr(GenIns2, "exposure") <- expos
-  checkException(fit5 <- glmReserve(GenIns2))
+  #checkException(fit5 <- glmReserve(GenIns2))
   names(expos) <- rownames(GenIns2)
   attr(GenIns2, "exposure") <- expos
   (fit6 <- glmReserve(GenIns2))
