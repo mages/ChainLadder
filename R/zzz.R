0978e1d509099c5a57780d065269648863b4c37f
Markus Gesmann <markus.gesmann@gmail.com>
2020-12-28 07:55:43 +0000

    Added packages as prefix

--- R/zzz.R
+++ R/zzz.R
@@ -5,10 +5,10 @@
 
 
 chainladderWelcomeMessage <- function(){
-  cit <- citation("ChainLadder")
+  cit <- utils::citation("ChainLadder")
   txt <- paste(c(format(cit,"citation")),collapse="\n\n")
-  cit_txt <- strsplit(txt, "A BibTeX")[[1]][1]
-  paste("\nWelcome to ChainLadder version ", packageDescription("ChainLadder")$Version,
+  cit_txt <- base::strsplit(txt, "A BibTeX")[[1]][1]
+  paste("\nWelcome to ChainLadder version ", utils::packageDescription("ChainLadder")$Version,
         cit_txt,
         "To suppress this message use:\n",
         "suppressPackageStartupMessages(library(ChainLadder))\n",       
