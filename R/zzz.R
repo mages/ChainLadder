4b2624be20dac73e22e8549b75cfac794e8bc8e1
Markus Gesmann <markus.gesmann@gmail.com>
2022-08-26 18:23:57 +0100

    Added linebreak to startup message. Increased version number

--- R/zzz.R
+++ R/zzz.R
@@ -9,7 +9,7 @@ chainladderWelcomeMessage <- function(){
   txt <- paste(c(format(cit,"citation")),collapse="\n\n")
   cit_txt <- base::strsplit(txt, "A BibTeX")[[1]][1]
   paste("\nWelcome to ChainLadder version ", utils::packageDescription("ChainLadder")$Version,
-        cit_txt,
+        "\n", cit_txt,
         "To suppress this message use:\n",
         "suppressPackageStartupMessages(library(ChainLadder))\n",       
         sep='')
