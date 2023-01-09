e8722414324b6ff0efa670b713870ca0a7f7262e
Markus Gesmann <markus.gesmann@gmail.com>
2023-01-09 13:55:55 +0000

    Added unit test for sampling triangles

--- inst/unitTests/runit.BootChainLadder.R
+++ inst/unitTests/runit.BootChainLadder.R
@@ -3,3 +3,10 @@ test.print.BootChainLadder <- function() {
   res <- BootChainLadder(RAA)
   checkEquals(res, print(res))
 }
+
+test.seed.BootChainLadder <- function() {
+  # Test that print(x) returns x; see ?print
+  B1 <- BootChainLadder(RAA, R = 10, seed = 123) 
+  B2 <- BootChainLadder(RAA, R = 100, seed = 123) 
+  checkEquals(B1$simClaims[,,1], B2$simClaims[,,1])
+}
